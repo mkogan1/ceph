36f419b9ba025b06d265c5a182aa1731786b4563
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-12-28 17:12:50 +0100

    rgw: convey placement rule in RGWBucketEnt and cls_user_bucket_entry.
    Fixes: http://tracker.ceph.com/issues/17932
    Resolves (with following): rhbz#1436386
    
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>
    (cherry picked from commit 5eca89f08d29ac6aa7b55a3b4fb5b4183bdc0496)

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -1898,7 +1898,8 @@ struct RGWBucketEnt {
       size(e.size),
       size_rounded(e.size_rounded),
       creation_time(e.creation_time),
-      count(e.count) {
+      count(e.count),
+      placement_rule(std::move(e.placement_rule)) {
   }
 
   RGWBucketEnt& operator=(const RGWBucketEnt&) = default;
@@ -1912,7 +1913,7 @@ struct RGWBucketEnt {
   }
 
   void encode(bufferlist& bl) const {
-    ENCODE_START(6, 5, bl);
+    ENCODE_START(7, 5, bl);
     uint64_t s = size;
     __u32 mt = ceph::real_clock::to_time_t(creation_time);
     string empty_str;  // originally had the bucket name here, but we encode bucket later
@@ -1924,6 +1925,7 @@ struct RGWBucketEnt {
     s = size_rounded;
     ::encode(s, bl);
     ::encode(creation_time, bl);
+    ::encode(placement_rule, bl);
     ENCODE_FINISH(bl);
   }
   void decode(bufferlist::iterator& bl) {
@@ -1947,6 +1949,8 @@ struct RGWBucketEnt {
     size_rounded = s;
     if (struct_v >= 6)
       ::decode(creation_time, bl);
+    if (struct_v >= 7)
+      ::decode(placement_rule, bl);
     DECODE_FINISH(bl);
   }
   void dump(Formatter *f) const;
