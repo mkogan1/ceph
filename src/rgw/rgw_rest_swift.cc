b998012889dbefb651a8e4647f4ccaa501c98439
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2017-05-30 22:19:33 +0200

    rgw: Swift API returns 400 Bad Request on too long container names.
    Fixes: http://tracker.ceph.com/issues/17935
    Resolves (with following): rhbz#1436386
    
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>
    (cherry picked from commit 1845e41292696da9e20ecd87b9260b44ee312aed)

--- src/rgw/rgw_rest_swift.cc
+++ src/rgw/rgw_rest_swift.cc
@@ -2647,11 +2647,22 @@ int RGWHandler_REST_SWIFT::postauth_init()
 
 int RGWHandler_REST_SWIFT::validate_bucket_name(const string& bucket)
 {
-  int ret = RGWHandler_REST::validate_bucket_name(bucket);
-  if (ret < 0)
-    return ret;
+  const size_t len = bucket.size();
 
-  int len = bucket.size();
+  if (len > MAX_BUCKET_NAME_LEN) {
+    /* Bucket Name too long. Generate custom error message and bind it
+     * to an R-value reference. */
+    const auto msg = boost::str(
+      boost::format("Container name length of %lld longer than %lld")
+        % len % int(MAX_BUCKET_NAME_LEN));
+    set_req_state_err(s, ERR_INVALID_BUCKET_NAME, msg);
+    return -ERR_INVALID_BUCKET_NAME;
+  }
+
+  const auto ret = RGWHandler_REST::validate_bucket_name(bucket);
+  if (ret < 0) {
+    return ret;
+  }
 
   if (len == 0)
     return 0;
@@ -2664,7 +2675,7 @@ int RGWHandler_REST_SWIFT::validate_bucket_name(const string& bucket)
 
   const char *s = bucket.c_str();
 
-  for (int i = 0; i < len; ++i, ++s) {
+  for (size_t i = 0; i < len; ++i, ++s) {
     if (*(unsigned char *)s == 0xff)
       return -ERR_INVALID_BUCKET_NAME;
   }
