6871912f2e9332a46109fbf8cb754c83a5096b82
Patrick Donnelly <pdonnell@redhat.com>
2018-05-17 17:06:52 -0700

    client: delay dentry trimming until after cap traversal
    Fixes: http://tracker.ceph.com/issues/24137
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit 9199179799b41d9534b27bcceefc1bc9f2763bac)
    (cherry picked from commit bc11928ac2fe29dbd89f47a69524878111302eb7)
    Resolves: rhbz#1578275

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -4084,9 +4084,10 @@ void Client::trim_caps(MetaSession *s, int max)
   ldout(cct, 10) << "trim_caps mds." << mds << " max " << max 
     << " caps " << caps_size << dendl;
 
-  int trimmed = 0;
-  xlist<Cap*>::iterator p = s->caps.begin();
-  std::set<InodeRef> anchor; /* prevent put_inode from deleting all caps during traversal */
+  uint64_t trimmed = 0;
+  auto p = s->caps.begin();
+  std::set<Dentry *> to_trim; /* this avoids caps other than the one we're
+                               * looking at from getting deleted during traversal. */
   while ((caps_size - trimmed) > max && !p.end()) {
     Cap *cap = *p;
     InodeRef in(cap->inode);
@@ -4101,8 +4102,7 @@ void Client::trim_caps(MetaSession *s, int max)
       // disposable non-auth cap
       if (!(get_caps_used(in.get()) & ~oissued & mine)) {
 	ldout(cct, 20) << " removing unused, unneeded non-auth cap on " << *in << dendl;
-	remove_cap(cap, true);
-        /* N.B. no need to push onto anchor, as we are only removing one cap */
+	cap = (remove_cap(cap, true), nullptr);
 	trimmed++;
       }
     } else {
@@ -4119,9 +4119,8 @@ void Client::trim_caps(MetaSession *s, int max)
 	    // the end of this function.
 	    _schedule_invalidate_dentry_callback(dn, true);
 	  }
-          ldout(cct, 20) << " anchoring inode: " << in->ino << dendl;
-          anchor.insert(in);
-	  trim_dentry(dn);
+          ldout(cct, 20) << " queueing dentry for trimming: " << dn->name << dendl;
+          to_trim.insert(dn);
         } else {
           ldout(cct, 20) << "  not expirable: " << dn->name << dendl;
 	  all = false;
@@ -4133,8 +4132,11 @@ void Client::trim_caps(MetaSession *s, int max)
       }
     }
   }
-  ldout(cct, 20) << " clearing anchored inodes" << dendl;
-  anchor.clear();
+  ldout(cct, 20) << " trimming queued dentries: " << dendl;
+  for (const auto &dn : to_trim) {
+    trim_dentry(dn);
+  }
+  to_trim.clear();
 
   caps_size = s->caps.size();
   if (caps_size > max)
