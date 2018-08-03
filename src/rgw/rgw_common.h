2ce8e357c4789eaee9028bea24d7473ba365e839
Marcus Watts <mwatts@redhat.com>
2018-08-02 23:49:30 -0400

    Add several types to ceph-dencoder.
    Add types: RGWBucketEntryPoint obj_version rgw_user
    These are structures that are visible as data at rest
    inside of rados when a bucket is made via radosgw.
    
    RGWBucketEntryPoint is the contents of a rados
    object with names that may be either "<bucket-name>"
    or "<tenant>/<bucket-name>"
    
    rgw_user is a structure contained inside of RGWBucketEntryPoint
    and other structures.
    
    obj_version is visible as the xattr "ceph.objclass.version"
    on rados objects in ".rgw.meta" that contain ".bucket.meta."
    
    Fixes: http://tracker.ceph.com/issues/35885
    Resolves: rhbz#1763219
    
    Signed-off-by: Marcus Watts <mwatts@redhat.com>
    (cherry picked from commit f7236ba1f7923eb643bbeb8bf861fc80dc915d6a)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -1596,6 +1596,7 @@ struct RGWBucketEntryPoint
 
   void dump(Formatter *f) const;
   void decode_json(JSONObj *obj);
+  static void generate_test_instances(list<RGWBucketEntryPoint*>& o);
 };
 WRITE_CLASS_ENCODER(RGWBucketEntryPoint)
 
