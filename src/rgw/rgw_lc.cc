f71efb07ff307b1b5dcefa6c7bafcfa08de14d12
Matt Benjamin <mbenjamin@redhat.com>
2019-02-19 11:17:45 -0500

    rgw: complete expiration header (object tags)
    The expiration header tag processing is complete, but the
    passed RGWObjTags argument was never initialized.  Now it is
    initialized in RGWGetObj and RGWPutObj, as required.
    
    Resolves: rhbz#1793160
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 8981c5e9f688b13a00345e069c2ce1e62fb0a588)

--- src/rgw/rgw_lc.cc
+++ src/rgw/rgw_lc.cc
@@ -1480,7 +1480,7 @@ std::string s3_expiration_header(
   const std::map<std::string, buffer::list>& bucket_attrs)
 {
   CephContext* cct = dpp->get_cct();
-  RGWLifecycleConfiguration config(cct); // TODO: save in bucket info
+  RGWLifecycleConfiguration config(cct);
   std::string hdr{""};
 
   const auto& aiter = bucket_attrs.find(RGW_ATTR_LC);
@@ -1574,8 +1574,7 @@ std::string s3_expiration_header(
     // update earliest expiration
     if (rule_expiration_date) {
       if ((! expiration_date) ||
-	  ((expiration_date &&
-	    (*expiration_date < *rule_expiration_date)))) {
+	  (*expiration_date < *rule_expiration_date)) {
       expiration_date =
 	boost::optional<ceph::real_time>(rule_expiration_date);
       }
