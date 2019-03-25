9ffa32906d3eb25e6962045aea0382e066712d06
Pritha Srivastava <prsrivas@redhat.com>
2019-03-25 12:37:23 +0530

    rgw: Adding 'iam' namespace for Role and User Policy related REST APIs.
    Resolves: rhbz#1765430
    
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>
    (cherry picked from commit 121a2695544bd56973459fad9b4f545b4ee412f8)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_rest_s3.h
+++ src/rgw/rgw_rest_s3.h
@@ -521,8 +521,10 @@ public:
 
 class RGWHandler_REST_Service_S3 : public RGWHandler_REST_S3 {
 protected:
-    const bool isSTSenabled;
-    const bool isPSenabled;
+  const bool isSTSenabled;
+  const bool isIAMenabled;
+  const bool isPSenabled;
+
     bool is_usage_op() {
     return s->info.args.exists("usage");
   }
@@ -531,8 +533,8 @@ protected:
   RGWOp *op_post() override;
 public:
    RGWHandler_REST_Service_S3(const rgw::auth::StrategyRegistry& auth_registry,
-                              bool _isSTSenabled, bool _isPSenabled) :
-      RGWHandler_REST_S3(auth_registry), isSTSenabled(_isSTSenabled), isPSenabled(_isPSenabled) {}
+                              bool _isSTSenabled, bool isIAMenabled, bool _isPSenabled) :
+     RGWHandler_REST_S3(auth_registry), isSTSenabled(_isSTSenabled), isIAMenabled(_isIAMenabled), isPSenabled(_isPSenabled) {}
   ~RGWHandler_REST_Service_S3() override = default;
 };
 
@@ -605,12 +607,14 @@ class RGWRESTMgr_S3 : public RGWRESTMgr {
 private:
   bool enable_s3website;
   bool enable_sts;
+  bool enable_iam;
   const bool enable_pubsub;
 public:
-  explicit RGWRESTMgr_S3(bool enable_s3website = false, bool enable_sts = false, bool _enable_pubsub = false)
+  explicit RGWRESTMgr_S3(bool enable_s3website = false, bool enable_sts = false, bool enable_iam = false, bool enable_pubsub = false)
     : enable_s3website(enable_s3website),
       enable_sts(enable_sts),
-      enable_pubsub(_enable_pubsub) {
+      enable_iam(enable_iam)
+      enable_pubsub(enable_pubsub) {
   }
 
   ~RGWRESTMgr_S3() override = default;
