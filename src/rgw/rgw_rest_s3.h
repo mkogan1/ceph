2d7b0c76a60e19f64d4dd47c121b0415b8f53cc6
Chang Liu <liuchang0812@gmail.com>
2019-05-17 10:53:32 +0800

    rgw: cleanup bucket tags code
    fixes:rhbz#1992145
    
    Signed-off-by: Chang Liu <liuchang0812@gmail.com>
    (cherry picked from commit d1ff8218a167d33851956ae9a845f9e5240c6a80)

--- src/rgw/rgw_rest_s3.h
+++ src/rgw/rgw_rest_s3.h
@@ -87,18 +87,12 @@ class RGWGetBucketTags_ObjStore_S3 : public RGWGetBucketTags_ObjStore
 {
   bufferlist tags_bl;
 public:
-  RGWGetBucketTags_ObjStore_S3() = default;
-  ~RGWGetBucketTags_ObjStore_S3() = default;
-
   void send_response_data(bufferlist &bl) override;
 };
 
 class RGWPutBucketTags_ObjStore_S3 : public RGWPutBucketTags_ObjStore
 {
 public:
-  RGWPutBucketTags_ObjStore_S3() {}
-  ~RGWPutBucketTags_ObjStore_S3() {}
-
   int get_params() override;
   void send_response() override;
 };
@@ -106,7 +100,6 @@ public:
 class RGWDeleteBucketTags_ObjStore_S3 : public RGWDeleteBucketTags
 {
 public:
-  ~RGWDeleteBucketTags_ObjStore_S3() override {}
   void send_response() override;
 };
 
