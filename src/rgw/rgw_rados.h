25b4b65b09c3b3693d5f5c5838d3667b99ddc79a
Casey Bodley <cbodley@redhat.com>
2019-05-24 16:59:34 -0400

    radosgw-admin: fix syncs_from in 'bucket sync status'
    If a zone is set up with sync_from_all=false, the bucket sync status
    command will incorrectly report "not in sync_from" because it's calling
    'zone.syncs_from(source.id)', where syncs_from() expects a zone name
    instead of id
    
    Fixes: http://tracker.ceph.com/issues/40022
    Resolves: rhbz#1713798
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 9da888686c3a8880976cd604bdafc4361068c985)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -1405,9 +1405,10 @@ struct RGWZone {
 
   bool is_read_only() { return read_only; }
 
-  bool syncs_from(const string& zone_id) const {
-    return (sync_from_all || sync_from.find(zone_id) != sync_from.end());
+  bool syncs_from(const std::string& zone_name) const {
+    return (sync_from_all || sync_from.find(zone_name) != sync_from.end());
   }
+
 };
 WRITE_CLASS_ENCODER(RGWZone)
 
