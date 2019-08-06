af3e6ba7a0230d31e88bbf5cd134bb5701c83d1a
Casey Bodley <cbodley@redhat.com>
2019-08-06 16:48:52 -0400

    kv/rocksdb: support rmrange unconditionally
    removes the config options rocksdb_enable_rmrange and
    rocksdb_max_items_rmrange that avoid calls to DeleteRange()
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 49672ad5c459b98d8dec94db54c2cb5b7da5ce8f)
    
    Conflicts:
    	src/common/options.cc
     - Whitespace
    
    Resolves: rhbz#2089634
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/kv/RocksDBStore.h
+++ src/kv/RocksDBStore.h
@@ -120,8 +120,6 @@ public:
   /// compact the underlying rocksdb store
   bool compact_on_mount;
   bool disableWAL;
-  bool enable_rmrange;
-  const uint64_t max_items_rmrange;
   void compact() override;
 
   void compact_async() override {
@@ -164,9 +162,7 @@ public:
     compact_queue_stop(false),
     compact_thread(this),
     compact_on_mount(false),
-    disableWAL(false),
-    enable_rmrange(cct->_conf->rocksdb_enable_rmrange),
-    max_items_rmrange(cct->_conf.get_val<uint64_t>("rocksdb_max_items_rmrange"))
+    disableWAL(false)
   {}
 
   ~RocksDBStore() override;
