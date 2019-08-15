77f40dabe4e9e4244218c613c7afa21a8cca360c
Mark Kogan <mkogan@redhat.com>
2019-08-15 09:59:39 -0400

    rgw: fix cls_bucket_list_unordered() partial results returnied after -ENOENT dirent is encountered
    (details in tracker issue below)
    
    fixes: https://tracker.ceph.com/issues/41297
    Resolves: rhbz#1750115
    
    Signed-off-by: Mark Kogan <mkogan@redhat.com>
    (cherry picked from commit 06dcfb1bd3390d6b169342b624c0ba4d62210bbf)

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -13557,6 +13557,8 @@ int RGWRados::cls_bucket_list_ordered(RGWBucketInfo& bucket_info,
       if (r < 0 && r != -ENOENT) {
           return r;
       }
+    } else {
+        r = 0;
     }
     if (r >= 0) {
       ldout(cct, 10) << "RGWRados::cls_bucket_list_ordered: got " <<
@@ -13697,6 +13699,8 @@ int RGWRados::cls_bucket_list_unordered(RGWBucketInfo& bucket_info,
 	if (r < 0 && r != -ENOENT) {
 	  return r;
 	}
+      } else {
+        r = 0;
       }
 
       // at this point either r >=0 or r == -ENOENT
