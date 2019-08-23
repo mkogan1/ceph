dc8878044da6ee34d6f7f96f57de9c287c7ebddf
Casey Bodley <cbodley@redhat.com>
2019-08-23 10:26:43 -0400

    rgw: remove bucket reshard checks from write operations
    we shouldn't pay for the latency of check_bucket_shards() or
    add_bucket_to_reshard() on write ops
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 84819d7484a9d34a5fee3497f9fdd70030f4c905)
    
    Conflicts: RGWRadosStore
    	src/rgw/rgw_file.cc
    	src/rgw/rgw_op.cc
    Resolves: rhbz#1763251

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -3707,11 +3707,6 @@ void RGWPutObj::execute()
       ldpp_dout(this, 20) << "check_quota() returned ret=" << op_ret << dendl;
       return;
     }
-    op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
-    if (op_ret < 0) {
-      ldpp_dout(this, 20) << "check_bucket_shards() returned ret=" << op_ret << dendl;
-      return;
-    }
   }
 
   if (supplied_etag) {
@@ -3911,12 +3906,6 @@ void RGWPutObj::execute()
     return;
   }
 
-  op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
-  if (op_ret < 0) {
-    ldpp_dout(this, 20) << "check_bucket_shards() returned ret=" << op_ret << dendl;
-    return;
-  }
-
   hash.Final(m);
 
   if (compressor && compressor->is_compressed()) {
@@ -4096,11 +4085,6 @@ void RGWPostObj::execute()
       return;
     }
 
-    op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
-    if (op_ret < 0) {
-      return;
-    }
-
     if (supplied_md5_b64) {
       char supplied_md5_bin[CEPH_CRYPTO_MD5_DIGESTSIZE + 1];
       ldpp_dout(this, 15) << "supplied_md5_b64=" << supplied_md5_b64 << dendl;
@@ -4200,11 +4184,6 @@ void RGWPostObj::execute()
 
     op_ret = store->check_quota(s->bucket_owner.get_id(), s->bucket,
                                 user_quota, bucket_quota, s->obj_size);
-    if (op_ret < 0) {
-      return;
-    }
-
-    op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
     if (op_ret < 0) {
       return;
     }
@@ -7025,11 +7004,6 @@ int RGWBulkUploadOp::handle_file(const boost::string_ref path,
     return op_ret;
   }
 
-  op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
-  if (op_ret < 0) {
-    return op_ret;
-  }
-
   rgw_obj obj(binfo.bucket, object);
   if (s->bucket_info.versioning_enabled()) {
     store->gen_rand_obj_instance_name(&obj);
@@ -7110,11 +7084,6 @@ int RGWBulkUploadOp::handle_file(const boost::string_ref path,
 			      user_quota, bucket_quota, size);
   if (op_ret < 0) {
     ldpp_dout(this, 20) << "quota exceeded for path=" << path << dendl;
-    return op_ret;
-  }
-
-  op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
-  if (op_ret < 0) {
     return op_ret;
   }
 
