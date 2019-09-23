11e56e509465fd4cede0b875b44dde0c254ac916
J. Eric Ivancich <ivancich@redhat.com>
2019-09-23 03:33:01 +0000

    rgw: clean up RGWOrphansSearch return values
    In some error conditions a positive rather than a negative value is
    returned to indicate an error state.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>
    
    Resolves: rhbz#1752130

--- src/rgw/rgw_orphan.cc
+++ src/rgw/rgw_orphan.cc
@@ -388,7 +388,7 @@ int RGWOrphanSearch::build_buckets_instance_index()
   int ret = store->meta_mgr->list_keys_init(section, &handle);
   if (ret < 0) {
     lderr(store->ctx()) << "ERROR: can't get key: " << cpp_strerror(-ret) << dendl;
-    return -ret;
+    return ret;
   }
 
   map<int, list<string> > instances;
@@ -405,7 +405,7 @@ int RGWOrphanSearch::build_buckets_instance_index()
     ret = store->meta_mgr->list_keys_next(handle, max, keys, &truncated);
     if (ret < 0) {
       lderr(store->ctx()) << "ERROR: lists_keys_next(): " << cpp_strerror(-ret) << dendl;
-      return -ret;
+      return ret;
     }
 
     for (list<string>::iterator iter = keys.begin(); iter != keys.end(); ++iter) {
@@ -527,7 +527,7 @@ int RGWOrphanSearch::build_linked_oids_for_bucket(const string& bucket_instance_
     ret = list_op.list_objects(MAX_LIST_OBJS_ENTRIES, &result, NULL, &truncated);
     if (ret < 0) {
       cerr << "ERROR: store->list_objects(): " << cpp_strerror(-ret) << std::endl;
-      return -ret;
+      return ret;
     }
 
     for (vector<rgw_bucket_dir_entry>::iterator iter = result.begin(); iter != result.end(); ++iter) {
