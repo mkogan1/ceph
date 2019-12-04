fa61a2370b92361a519658ef89ab0ecbe3f3f9a6
Mykola Golub <mgolub@suse.com>
2019-12-04 09:31:48 +0000

    librbd: introduce LIST_WATCHERS_MIRROR_INSTANCES_ONLY flag
    Also, don't skip listing mirror watchers if the image has journaling
    disabled -- it is not correct for snapshot mirror mode.
    
    Signed-off-by: Mykola Golub <mgolub@suse.com>
    (cherry picked from commit 3c780822827911d3e61772e89bc91aa1b4c25686)
    
    Resolves: rhbz#1860073

--- src/librbd/image/ListWatchersRequest.cc
+++ src/librbd/image/ListWatchersRequest.cc
@@ -36,6 +36,8 @@ ListWatchersRequest<I>::ListWatchersRequest(I &image_ctx, int flags,
                                             Context *on_finish)
   : m_image_ctx(image_ctx), m_flags(flags), m_watchers(watchers),
     m_on_finish(on_finish), m_cct(m_image_ctx.cct) {
+  ceph_assert((m_flags & LIST_WATCHERS_FILTER_OUT_MIRROR_INSTANCES) == 0 ||
+              (m_flags & LIST_WATCHERS_MIRROR_INSTANCES_ONLY) == 0);
 }
 
 template<typename I>
@@ -83,8 +85,8 @@ void ListWatchersRequest<I>::handle_list_image_watchers(int r) {
 template<typename I>
 void ListWatchersRequest<I>::list_mirror_watchers() {
   if ((m_object_watchers.empty()) ||
-      (m_flags & LIST_WATCHERS_FILTER_OUT_MIRROR_INSTANCES) == 0 ||
-      (m_image_ctx.features & RBD_FEATURE_JOURNALING) == 0) {
+      (m_flags & (LIST_WATCHERS_FILTER_OUT_MIRROR_INSTANCES |
+                  LIST_WATCHERS_MIRROR_INSTANCES_ONLY)) == 0) {
     finish(0);
     return;
   }
@@ -139,15 +141,19 @@ void ListWatchersRequest<I>::finish(int r) {
             continue;
           }
         }
+        auto it = std::find_if(m_mirror_watchers.begin(),
+                               m_mirror_watchers.end(),
+                               [w] (obj_watch_t &watcher) {
+                                 return (strncmp(w.addr, watcher.addr,
+                                                 sizeof(w.addr)) == 0);
+                               });
         if ((m_flags & LIST_WATCHERS_FILTER_OUT_MIRROR_INSTANCES) != 0) {
-          auto it = std::find_if(m_mirror_watchers.begin(),
-                                 m_mirror_watchers.end(),
-                                 [w] (obj_watch_t &watcher) {
-                                   return (strncmp(w.addr, watcher.addr,
-                                                   sizeof(w.addr)) == 0);
-                                 });
           if (it != m_mirror_watchers.end()) {
             ldout(m_cct, 20) << "filtering out mirror instance: " << w << dendl;
+            continue;
+          }
+        } else if ((m_flags & LIST_WATCHERS_MIRROR_INSTANCES_ONLY) != 0) {
+          if (it == m_mirror_watchers.end()) {
             continue;
           }
         }
