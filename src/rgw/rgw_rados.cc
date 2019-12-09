ccf2f706b98c043f1246d58d840337bff2763999
Matt Benjamin <mbenjamin@redhat.com>
2019-12-08 20:10:40 -0500

    rgw: avoid 0-length result gaps when non-vis b-i entries dominate
    Attempt a workaround for invalid, empty bucket listing results
    induced by sharded-bucket readahead optimizations.
    
    The proximate cause if failure is failure to return any results
    due to sparse/empty visible entry counts in some shards, where
    non-visible entries are present.
    
    The current change addresses the currently reproducible
    case, plus cognate cases with other !visible entries,
    which were being handled similarly.
    
    changes suggested in reviews by Eric and Casey:
    
    * there is no fixed bound on restarts, so long as marker advances
    * marker advance can/should be moved ahead of remaining continue case
    * rename check_ns (which is the result of '(params.ns == objs.ns)'),
      hence means something like 'namespaces match'
    
    Fixes: https://tracker.ceph.com/issues/43180
    Resolves: rhbz#1781358
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 653b71e7d018881250153205548d0b5a090e41a3)

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -2417,6 +2417,7 @@ int RGWRados::Bucket::List::list_objects_ordered(
 
   result->clear();
 
+restart:
   rgw_obj_key marker_obj(params.marker.name, params.marker.instance, params.ns);
   rgw_obj_index_key cur_marker;
   marker_obj.get_index_key(&cur_marker);
@@ -2476,36 +2477,7 @@ int RGWRados::Bucket::List::list_objects_ordered(
     for (auto eiter = ent_map.begin(); eiter != ent_map.end(); ++eiter) {
       rgw_bucket_dir_entry& entry = eiter->second;
       rgw_obj_index_key index_key = entry.key;
-
       rgw_obj_key obj(index_key);
-
-      /* note that parse_raw_oid() here will not set the correct
-       * object's instance, as rgw_obj_index_key encodes that
-       * separately. We don't need to set the instance because it's
-       * not needed for the checks here and we end up using the raw
-       * entry for the return vector
-       */
-      bool valid = rgw_obj_key::parse_raw_oid(index_key.name, &obj);
-      if (!valid) {
-        ldout(cct, 0) << "ERROR: could not parse object name: " << obj.name << dendl;
-        continue;
-      }
-
-      bool check_ns = (obj.ns == params.ns);
-      if (!params.list_versions && !entry.is_visible()) {
-        continue;
-      }
-
-      if (params.enforce_ns && !check_ns) {
-        if (!params.ns.empty()) {
-          /* we've iterated past the namespace we're searching -- done now */
-          truncated = false;
-          goto done;
-        }
-
-        /* we're not looking at the namespace this object is in, next! */
-        continue;
-      }
 
       if (cur_end_marker_valid && cur_end_marker <= index_key) {
         truncated = false;
@@ -2515,6 +2487,35 @@ int RGWRados::Bucket::List::list_objects_ordered(
       if (count < max) {
         params.marker = index_key;
         next_marker = index_key;
+      }
+
+      /* note that parse_raw_oid() here will not set the correct
+       * object's instance, as rgw_obj_index_key encodes that
+       * separately. We don't need to set the instance because it's
+       * not needed for the checks here and we end up using the raw
+       * entry for the return vector
+       */
+      bool valid = rgw_obj_key::parse_raw_oid(index_key.name, &obj);
+      if (!valid) {
+        ldout(cct, 0) << "ERROR: could not parse object name: "
+		      << obj.name << dendl;
+        continue;
+      }
+
+      bool matched_ns = (obj.ns == params.ns);
+      if (!params.list_versions && !entry.is_visible()) {
+        continue;
+      }
+
+      if (params.enforce_ns && !matched_ns) {
+        if (!params.ns.empty()) {
+          /* we've iterated past the namespace we're searching -- done now */
+          truncated = false;
+          goto done;
+        }
+
+        /* we're not looking at the namespace this object is in, next! */
+        continue;
       }
 
       if (params.filter && !params.filter->filter(obj.name, index_key.name))
@@ -2578,6 +2579,11 @@ int RGWRados::Bucket::List::list_objects_ordered(
   } // read attempt loop
 
 done:
+  /* reject empty results */
+  if (result->size() == 0) {
+    goto restart;
+  }
+
   if (is_truncated)
     *is_truncated = truncated;
 
