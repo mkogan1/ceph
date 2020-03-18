4e781d233715cf022a7845976dc282a25785ddd4
J. Eric Ivancich <ivancich@redhat.com>
2020-03-18 10:34:23 -0400

    rgw: clean up attempt loop in ordered listing
    Convert a while loop into a for loop to better indicate how the
    "attempt" lcv is managed.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>
    (cherry picked from commit 6ef83820564b07dbff91b3fc6909082537c0f815)
    
    Resolves: rhbz#1853052

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -2456,14 +2456,9 @@ int RGWRados::Bucket::List::list_objects_ordered(
 
   string skip_after_delim;
   rgw_obj_index_key prev_marker;
-  uint16_t attempt = 0;
-  while (true) {
+  for (uint16_t attempt = 1; /* empty */; ++attempt) {
     ldout(cct, 20) << "RGWRados::Bucket::List::" << __func__ <<
-      " beginning attempt=" << ++attempt << dendl;
-
-    // this loop is generally expected only to have a single
-    // iteration; the standard exit is at the bottom of the loop, but
-    // there's an error condition emergency exit as well
+      " starting attempt " << attempt << dendl;
 
     if (attempt > 1 && !(prev_marker < cur_marker)) {
       // we've failed to make forward progress
@@ -2612,11 +2607,7 @@ int RGWRados::Bucket::List::list_objects_ordered(
 	break;
       }
     }
-
-    ldout(cct, 1) << "RGWRados::Bucket::List::" << __func__ <<
-      " INFO ordered bucket listing requires read #" << (1 + attempt) <<
-      dendl;
-  } // read attempt loop
+  } // for (uint16_t attempt...
 
 done:
 
