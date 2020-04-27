c1482809f66db9db8066fe1628e0af0276bb1c9e
Matt Benjamin <mbenjamin@redhat.com>
2020-04-27 19:07:47 -0400

    rgw: nfs: disable gc, lc, quota, and sync threads by default
    These can be overridden by new options, should a site decide to
    run a librgw with no radosgw instances.
    
    Resolves: rhbz#1794715
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry-picked from commit ccff9f122b94e25f1c6947cbc08a51577ccc6431)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/librgw.cc
+++ src/rgw/librgw.cc
@@ -499,11 +499,27 @@ namespace rgw {
     rgw_init_resolver();
     rgw::curl::setup_curl(boost::none);
 
+    auto run_gc =
+      g_conf->rgw_enable_gc_threads &&
+      g_conf->rgw_nfs_run_gc_threads;
+
+    auto run_lc =
+      g_conf->rgw_enable_lc_threads &&
+      g_conf->rgw_nfs_run_lc_threads;
+
+    auto run_quota =
+      g_conf->rgw_enable_quota_threads &&
+      g_conf->rgw_nfs_run_quota_threads;
+
+    auto run_sync =
+      g_conf->rgw_run_sync_thread &&
+      g_conf->rgw_nfs_run_sync_thread;
+
     store = RGWStoreManager::get_storage(g_ceph_context,
-					 g_conf->rgw_enable_gc_threads,
-					 g_conf->rgw_enable_lc_threads,
-					 g_conf->rgw_enable_quota_threads,
-					 g_conf->rgw_run_sync_thread,
+					 run_gc,
+					 run_lc,
+					 run_quota,
+					 run_sync,
 					 g_conf->get_val<bool>("rgw_dynamic_resharding"));
 
     if (!store) {
