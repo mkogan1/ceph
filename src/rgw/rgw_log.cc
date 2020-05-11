ef860d8d7cf1a67fc77c3e40ace94b244ac6071d
Pritha Srivastava <prsrivas@redhat.com>
2020-05-11 23:22:02 +0530

    rgw: adding user related web token claims to ops log for auditing purposes.
    Fixes: https://tracker.ceph.com/issues/45989
    Fixes: rhbz#1965540
    
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>
    (cherry picked from commit bb6a68ec14f07c17fdf68243aaec35c17d5e01bf)

--- src/rgw/rgw_log.cc
+++ src/rgw/rgw_log.cc
@@ -273,6 +273,21 @@ void rgw_format_ops_log_entry(struct rgw_log_entry& entry, Formatter *formatter)
     }
     formatter->close_section();
   }
+  formatter->dump_string("trans_id", entry.trans_id);
+  if (entry.token_claims.size() > 0) {
+    if (entry.token_claims[0] == "sts") {
+      formatter->open_object_section("sts_token_claims");
+      for (const auto& iter: entry.token_claims) {
+        auto pos = iter.find(":");
+        if (pos != string::npos) {
+          string claim_name = iter.substr(0, pos);
+          formatter->dump_string(claim_name.c_str(), iter.substr(pos + 1));
+        }
+      }
+      formatter->close_section();
+    }
+  }
+
   formatter->close_section();
 }
 
@@ -392,6 +407,10 @@ int rgw_log_op(RGWRados *store, RGWREST* const rest, struct req_state *s,
   entry.uri = std::move(uri);
 
   entry.op = op_name;
+
+  if (! s->token_claims.empty()) {
+    entry.token_claims = std::move(s->token_claims);
+  }
 
   /* custom header logging */
   if (rest) {
