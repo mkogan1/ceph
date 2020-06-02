ec3b4f2d463deb7233cfafe4a3fbd5ef1d5350e2
Nizamudeen A <nia@redhat.com>
2020-06-02 13:18:37 +0530

    mgr/dashboard: branding: Navigation Bar
    Resolves: rhbz#1832493
    
    Signed-off-by: Nizamudeen A <nia@redhat.com>
    (cherry picked from commit 892e4913915be1e46c170daee36db008f48f7468)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/models/cd-notification.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/models/cd-notification.ts
@@ -25,11 +25,19 @@ export class CdNotificationConfig {
 export class CdNotification extends CdNotificationConfig {
   timestamp: string;
   textClass: string;
+  overrideTextClass: string;
   iconClass: string;
+  overrideIconClass: string;
   duration: number;
 
   private textClasses = ['text-danger', 'text-info', 'text-success'];
+  private overrtideTextClasses = ['pf-m-danger', 'pf-m-info', 'pf-m-success'];
   private iconClasses = [Icons.warning, Icons.info, Icons.check];
+  private overrideIconClasses = [
+    'fas fa-exclamation-circle',
+    'fas fa-info-circle',
+    'fas fa-check-circle'
+  ];
 
   constructor(private config: CdNotificationConfig = new CdNotificationConfig()) {
     super(config.type, config.title, config.message, config.options, config.application);
@@ -38,7 +46,9 @@ export class CdNotification extends CdNotificationConfig {
     with the timestamps parsed from localStorage */
     this.timestamp = new Date().toJSON();
     this.iconClass = this.iconClasses[this.type];
+    this.overrideIconClass = this.overrideIconClasses[this.type];
     this.textClass = this.textClasses[this.type];
+    this.overrideTextClass = this.overrtideTextClasses[this.type];
     this.isFinishedTask = config.isFinishedTask;
   }
 }
