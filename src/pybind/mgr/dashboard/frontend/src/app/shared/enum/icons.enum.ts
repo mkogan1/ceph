115a3e5c42b7deabea83dbe8081611ca6b4b62e7
Nizamudeen A <nia@redhat.com>
2020-06-22 15:42:36 +0530

    mgr/dashboard: branding: minor ui elements
    Resolves: rhbz#1832528
    
    Signed-off-by: Nizamudeen A <nia@redhat.com>
    (cherry picked from commit 80ea6dc6834089a893864bee27e9d1816f6a9524)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/enum/icons.enum.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/enum/icons.enum.ts
@@ -3,7 +3,7 @@ export enum Icons {
   add = 'fa fa-plus', // Create, Add
   addCircle = 'fa fa-plus-circle', // Plus with Circle
   minusCircle = 'fa fa-minus-circle', // Minus with Circle
-  edit = 'fa fa-pencil', // Edit, Edit Mode, Rename
+  edit = 'fa fa-pencil-alt', // Edit, Edit Mode, Rename
   destroy = 'fa fa-times', // Destroy, Remove, Delete
   destroyCircle = 'fa fa-times-circle', // Destroy, Remove, Delete
   exchange = 'fa fa-exchange', // Edit-Peer
@@ -32,7 +32,7 @@ export enum Icons {
   warning = 'fa fa-exclamation-triangle', // Notification warning
   info = 'fa fa-info', // Notification information
   infoCircle = 'fa fa-info-circle', // Info on landing page
-  questionCircle = 'fa fa-question-circle-o',
+  questionCircle = 'fa fa-question-circle',
   check = 'fa fa-check', // Notification check
   show = 'fa fa-eye', // Show
   paragraph = 'fa fa-paragraph', // Silence Matcher - Attribute name
