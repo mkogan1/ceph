918d87fb6356180705939deedaa36b396adb9921
J. Eric Ivancich <ivancich@redhat.com>
2020-06-23 16:25:05 -0400

    rgw: radoslist incomplete multipart uploads fix marker progression
    When the radosgw-admin radoslist was running, the portion where it
    lists the incomplete multipart uploads did not handle the marker
    correctly between calls to the ordered list op. This fixes those
    issues and does some additional clean-up.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>
    
    Resolves: rhbz#1831740

--- src/rgw/rgw_orphan.cc
+++ src/rgw/rgw_orphan.cc
@@ -1401,21 +1401,16 @@ int RGWRadosList::do_incomplete_multipart(
   int ret;
 
   RGWRados::Bucket target(store, bucket_info);
+
   RGWRados::Bucket::List list_op(&target);
+  list_op.params.ns = mp_ns;
+  list_op.params.filter = &mp_filter;
+  // use empty string for initial list_op.params.marker
+  // use empty strings for list_op.params.{prefix,delim}
 
   bool is_listing_truncated;
-  std::string empty_string;
-  RGWMultipartUploadEntry next_uploads_marker;
 
   do {
-    RGWMPObj uploads_marker = next_uploads_marker.mp;
-    const std::string& marker_meta = uploads_marker.get_meta();
-    list_op.params.marker = marker_meta;
-    list_op.params.ns = mp_ns;
-    list_op.params.filter = &mp_filter;
-
-    // use empty strings for list_op.params.{prefix,delim}
-
     std::vector<rgw_bucket_dir_entry> objs;
     std::map<string, bool> common_prefixes;
     ret = list_op.list_objects(max_uploads, &objs, &common_prefixes,
@@ -1447,10 +1442,8 @@ int RGWRadosList::do_incomplete_multipart(
 	  " processing incomplete multipart entry " <<
 	  entry << dendl;
       }
-      next_uploads_marker = entry;
 
       // now process the uploads vector
-
       int parts_marker = 0;
       bool is_parts_truncated = false;
       do {
@@ -1482,7 +1475,7 @@ int RGWRadosList::do_incomplete_multipart(
 	}
       } while (is_parts_truncated);
     } // if objs not empty
-  } while(is_listing_truncated);
+  } while (is_listing_truncated);
 
   return 0;
 } // RGWRadosList::do_incomplete_multipart
