7268d24d87b9a70af0407c9772aa777932a15269
Alfonso Martínez <almartin@redhat.com>
2020-07-03 13:27:40 +0200

    mgr/dashboard: branding: landing page
    Resolves: rhbz#1832500
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>
    (cherry picked from commit 8dab6e1ab0c7690ea988e9cc2ace371aea3e474e)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/health-pie/health-pie-color.enum.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/health-pie/health-pie-color.enum.ts
@@ -1,7 +1,13 @@
 export enum HealthPieColor {
-  DEFAULT_RED = '#ff7592',
-  DEFAULT_BLUE = '#1d699d',
-  DEFAULT_ORANGE = '#ffa500',
-  DEFAULT_MAGENTA = '#564d65',
-  DEFAULT_GREEN = '#00bb00'
+  DEFAULT_RED = '#c9190b',
+  DEFAULT_BLUE = '#0066cc', // --pf-chart-color-blue-300
+  DEFAULT_ORANGE = '#EF9234', // --pf-chart-color-orange-200
+  DEFAULT_YELLOW = '#F6D173', // --pf-chart-color-gold-200
+  DEFAULT_MAGENTA = '#009596',
+  DEFAULT_GREEN = '#7CC674', // --pf-chart-color-green-200
+  DEFAULT_GRAY = '#EDEDED', // https://www.patternfly.org/v4/design-guidelines/usage-and-behavior/charts#donut-chart---utilization
+  DEFAULT_LIGHT_BLUE = '#519DE9', // --pf-chart-color-blue-200
+  DEFAULT_LIGHT_YELLOW = '#F9E0A2', // --pf-chart-color-gold-100
+  DEFAULT_CYAN = '#73C5C5', // --pf-chart-color-cyan
+  DEFAULT_PURPLE = '#3C3D99' // --pf-chart-color-purple
 }
