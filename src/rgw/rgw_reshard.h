66115dc61adc93d20c3f673c8fa9054537e68f18
Shilpa Jagannath <smanjara@redhat.com>
2020-07-14 17:56:05 +0530

    rgw: failing to reshard, restore old indexes
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>
    (cherry picked from commit a9d7c86f42fe2584bfbdcfc705548f77a5f23716)
    
    Conflicts:
    	src/rgw/rgw_reshard.cc
    	src/rgw/rgw_reshard.h
     - Upstream namespacery, dpp, services
    
    Resolves: rhbz#1831737
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_reshard.h
+++ src/rgw/rgw_reshard.h
@@ -69,7 +69,8 @@ private:
 
   RGWRados *store;
   RGWBucketInfo bucket_info;
-  std::map<string, bufferlist> bucket_attrs;
+  std::map<std::string, bufferlist> bucket_attrs;
+  rgw::bucket_index_layout_generation prev_index;
 
   RGWBucketReshardLock reshard_lock;
   RGWBucketReshardLock* outer_reshard_lock;
