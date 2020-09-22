149c9c8a16ac33a42231ce4145067d3ceec16ac7
Marc Gariepy <gariepy.marc@gmail.com>
2020-09-22 09:01:33 -0400

    ceph-volume: remove unneeded call to get_devices()
    there is no need to probe the device to generate the argparse help
    message.
    
    also removing the test on the function as it's not there anymore.
    
    Signed-off-by: Marc Gariepy <gariepy.marc@gmail.com>
    Fixes: https://tracker.ceph.com/issues/47502
    (cherry picked from commit 5c6f66166a7afad87627032cafdc5c4f11f94eac)
    
    Resolves: rhbz#1892441

--- src/ceph-volume/ceph_volume/devices/lvm/batch.py
+++ src/ceph-volume/ceph_volume/devices/lvm/batch.py
@@ -2,7 +2,7 @@ import argparse
 import logging
 from textwrap import dedent
 from ceph_volume import terminal, decorators
-from ceph_volume.util import disk, prompt_bool
+from ceph_volume.util import prompt_bool
 from ceph_volume.util import arg_validators
 from . import strategies
 
@@ -123,9 +123,6 @@ class Batch(object):
     _help = dedent("""
     Automatically size devices ready for OSD provisioning based on default strategies.
 
-    Detected devices:
-    {detected_devices}
-
     Usage:
 
         ceph-volume lvm batch [DEVICE...]
@@ -137,18 +134,6 @@ class Batch(object):
 
     def __init__(self, argv):
         self.argv = argv
-
-    def get_devices(self):
-        # remove devices with partitions
-        devices = [(device, details) for device, details in
-                       disk.get_devices().items() if details.get('partitions') == {}]
-        size_sort = lambda x: (x[0], x[1]['size'])
-        return device_formatter(sorted(devices, key=size_sort))
-
-    def print_help(self):
-        return self._help.format(
-            detected_devices=self.get_devices(),
-        )
 
     def report(self, args):
         strategy = self._get_strategy(args)
@@ -191,7 +176,7 @@ class Batch(object):
         parser = argparse.ArgumentParser(
             prog='ceph-volume lvm batch',
             formatter_class=argparse.RawDescriptionHelpFormatter,
-            description=self.print_help(),
+            description=self._help,
         )
 
         parser.add_argument(
