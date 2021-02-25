8bb5e41ae9333e618ff9bb23b21489a99978c8fb
Casey Bodley <cbodley@redhat.com>
2021-02-25 15:39:26 -0500

    rgw: BILog_List handles requests for generation=0
    previous logic treated requests for generation=0 as the latest gen
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 5c58a5b9e7b311e8f143fd698b02fca0506c1793)
    
    Conflicts:
    	src/rgw/rgw_rest_log.cc
     - Dipipage
    
    Resolves: rhbz#1831737
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_rest_log.cc
+++ src/rgw/rgw_rest_log.cc
@@ -356,12 +356,13 @@ void RGWOp_MDLog_Notify::execute() {
 }
 
 void RGWOp_BILog_List::execute() {
+  bool gen_specified = false;
   string tenant_name = s->info.args.get("tenant"),
          bucket_name = s->info.args.get("bucket"),
          marker = s->info.args.get("marker"),
          max_entries_str = s->info.args.get("max-entries"),
          bucket_instance = s->info.args.get("bucket-instance"),
-         gen_str = s->info.args.get("generation"),
+         gen_str = s->info.args.get("generation", &gen_specified),
          format_version_str = s->info.args.get("format-ver");
   RGWBucketInfo bucket_info;
   unsigned max_entries;
@@ -373,11 +374,14 @@ void RGWOp_BILog_List::execute() {
   }
 
   string err;
-  const uint64_t gen = strict_strtoll(gen_str.c_str(), 10, &err);
-  if (!err.empty()) {
-    ldpp_dout(s, 5) << "Error parsing generation param " << gen_str << dendl;
-    op_ret = -EINVAL;
-    return;
+  std::optional<uint64_t> gen;
+  if (gen_specified) {
+    gen = strict_strtoll(gen_str.c_str(), 10, &err);
+    if (!err.empty()) {
+      ldpp_dout(s, 5) << "Error parsing generation param " << gen_str << dendl;
+      op_ret = -EINVAL;
+      return;
+    }
   }
 
   if (!format_version_str.empty()) {
@@ -412,9 +416,9 @@ void RGWOp_BILog_List::execute() {
   const auto& logs = bucket_info.layout.logs;
   auto log = std::prev(logs.end());
   if (gen) {
-    log = std::find_if(logs.begin(), logs.end(), rgw::matches_gen(gen));
+    log = std::find_if(logs.begin(), logs.end(), rgw::matches_gen(*gen));
     if (log == logs.end()) {
-      ldpp_dout(s, 5) << "ERROR: no log layout with gen=" << gen << dendl;
+      ldpp_dout(s, 5) << "ERROR: no log layout with gen=" << *gen << dendl;
       op_ret = -ENOENT;
       return;
     }
