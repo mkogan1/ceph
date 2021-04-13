bc53f4874e224ead8516a7847f906f05a991b2b1
Greg Farnum <gfarnum@redhat.com>
2021-04-13 06:25:47 +0000

    mon: set_healthy_stretch_mode in update_from_paxos, not random leader calls!
    Add header comment describing how this works now.
    
    Fixes: https://tracker.ceph.com/issues/50308
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>
    (cherry picked from commit adbaeeec959f6341540ba96cbad9e0c46c53a50f)
    
    Resolves: rhbz#1949166

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -6723,7 +6723,6 @@ void Monitor::go_recovery_stretch_mode()
   if (!osdmon()->is_writeable()) {
     osdmon()->wait_for_writeable_ctx(new CMonGoRecovery(this));
   }
-  set_recovery_stretch_mode();
   osdmon()->trigger_recovery_stretch_mode();
 }
 
@@ -6817,7 +6816,6 @@ void Monitor::trigger_healthy_stretch_mode()
   }
 
   ceph_assert(osdmon()->osdmap.recovering_stretch_mode);
-  set_healthy_stretch_mode();
   osdmon()->trigger_healthy_stretch_mode();
   monmon()->trigger_healthy_stretch_mode();
 }
