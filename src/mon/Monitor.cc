2729a3b77a5642d7775b86477531acd4eeb300cb
Greg Farnum <gfarnum@redhat.com>
2021-04-13 05:28:31 +0000

    mon: maintain stretch_recovery_triggered in new OSDMon::set_*_stretch_mode
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>
    (cherry picked from commit 73b461a4f3ab87e20b4df5cae292e70a85a65d69)
    
    Resolves: rhbz#1949166

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -6731,6 +6731,7 @@ void Monitor::set_recovery_stretch_mode()
 {
   degraded_stretch_mode = true;
   recovering_stretch_mode = true;
+  osdmon()->set_recovery_stretch_mode();
 }
 
 void Monitor::maybe_go_degraded_stretch_mode()
@@ -6791,6 +6792,7 @@ void Monitor::set_degraded_stretch_mode()
 {
   degraded_stretch_mode = true;
   recovering_stretch_mode = false;
+  osdmon()->set_degraded_stretch_mode();
 }
 
 struct CMonGoHealthy : public Context {
@@ -6824,6 +6826,7 @@ void Monitor::set_healthy_stretch_mode()
 {
   degraded_stretch_mode = false;
   recovering_stretch_mode = false;
+  osdmon()->set_healthy_stretch_mode();
 }
 
 bool Monitor::session_stretch_allowed(MonSession *s, MonOpRequestRef& op)
