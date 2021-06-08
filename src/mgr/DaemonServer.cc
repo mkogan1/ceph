sunnyku/ceph-ci

mds: send scrub status to ceph-mgr only when scrub is running
Signed-off-by: Venky Shankar <vshankar@redhat.com>

src/mgr/DaemonServer.cc
@@ -532,9 +532,21 @@ bool DaemonServer::handle_close(const ref_t<MMgrClose>& m)
 void DaemonServer::update_task_status(DaemonKey key, const ref_t<MMgrReport>& m) {
   dout(10) << "got task status from " << key << dendl;
 
-  auto p = pending_service_map.get_daemon(key.type, key.name);
-  if (!map_compare(p.first->task_status, *m->task_status)) {
-    p.first->task_status = *m->task_status;
+  bool service_map_dirty = false;
+  if ((*m->task_status).empty()) {
+    auto removed = pending_service_map.rm_daemon(key.type, key.name);
+    if (removed) {
+      service_map_dirty = true;
+    }
+  } else {
+    auto p = pending_service_map.get_daemon(key.type, key.name);
+    if (!map_compare(p.first->task_status, *m->task_status)) {
+      service_map_dirty = true;
+      p.first->task_status = *m->task_status;
+    }
+  }
+
+  if (service_map_dirty) {
     pending_service_map_dirty = pending_service_map.epoch;
   }
 }

mkogan1/ceph

Resolves: rhbz#1859181
(cherry picked from commit 5a7d0632d989536e09b2d916b5ea9713dc8dbea5)
Conflicts:
	src/mds/MDSRank.h
	src/mds/ScrubStack.h
	src/mgr/DaemonServer.cc
(cherry picked from commit f8676a2057be64d17681eb4b050dcb85c2eaa4b3)
In ceph-4.2 downstream, DaemonKey is of type std::map<string, string>
which is different in upstream nautilus where DaemonKey is a struct
with @type and @name member variables. So, adjust to that.

src/mgr/DaemonServer.cc
@@ -542,9 +542,21 @@ bool DaemonServer::handle_close(MMgrClose *m)
 void DaemonServer::update_task_status(DaemonKey key, MMgrReport *m) {
   dout(10) << "got task status from " << key << dendl;
 
-  auto p = pending_service_map.get_daemon(key.first, key.second);
-  if (!map_compare(p.first->task_status, *m->task_status)) {
-    p.first->task_status = *m->task_status;
+  bool service_map_dirty = false;
+  if ((*m->task_status).empty()) {
+    auto removed = pending_service_map.rm_daemon(key.first, key.second);
+    if (removed) {
+      service_map_dirty = true;
+    }
+  } else {
+    auto p = pending_service_map.get_daemon(key.first, key.second);
+    if (!map_compare(p.first->task_status, *m->task_status)) {
+      service_map_dirty = true;
+      p.first->task_status = *m->task_status;
+    }
+  }
+
+  if (service_map_dirty) {
     pending_service_map_dirty = pending_service_map.epoch;
   }
 }