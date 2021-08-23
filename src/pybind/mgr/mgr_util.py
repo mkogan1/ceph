f2f2f0fe151784b15948f82ff2d60fc20b507f0f
Sebastian Wagner <sewagner@redhat.com>
2021-08-23 10:33:37 +0200

    mgr/{prometheus,restful}: Fix url generation again
    Fixes
    
    ```
    ======================================================================
    ERROR: test_standby (tasks.mgr.test_prometheus.TestPrometheus)
    ----------------------------------------------------------------------
    urllib3.exceptions.LocationParseError: Failed to parse: http://172.21.15.71:7789metrics
    
    ```
    Signed-off-by: Sebastian Wagner <sewagner@redhat.com>
    (cherry picked from commit 45361ea9b3dab917a455bc37bc96aabd0bf64fcf)
    Resolves: rhbz#1975338

--- src/pybind/mgr/mgr_util.py
+++ src/pybind/mgr/mgr_util.py
@@ -405,7 +405,7 @@ def get_default_addr():
         return result
 
 
-def build_url(host: str, scheme: Optional[str] = None, port: Optional[int] = None) -> str:
+def build_url(host: str, scheme: Optional[str] = None, port: Optional[int] = None, path: str = '') -> str:
     """
     Build a valid URL. IPv6 addresses specified in host will be enclosed in brackets
     automatically.
@@ -418,6 +418,10 @@ def build_url(host: str, scheme: Optional[str] = None, port: Optional[int] = Non
 
     >>> build_url('fce:9af7:a667:7286:4917:b8d3:34df:8373', port=80, scheme='http')
     'http://[fce:9af7:a667:7286:4917:b8d3:34df:8373]:80'
+
+    >>> build_url('example.com', 'https', 443, path='/metrics')
+    'https://example.com:443/metrics'
+
 
     :param scheme: The scheme, e.g. http, https or ftp.
     :type scheme: str
@@ -433,7 +437,7 @@ def build_url(host: str, scheme: Optional[str] = None, port: Optional[int] = Non
     pr = urllib.parse.ParseResult(
         scheme=scheme if scheme else '',
         netloc=netloc,
-        path='',
+        path=path,
         params='',
         query='',
         fragment='')
