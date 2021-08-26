ac5937d9f6c2255eeb2b7a425a5f79f40a87cd77
Marcus Watts <mwatts@redhat.com>
2021-08-26 05:08:00 -0400

    rgw: remove unused routine in rgw_admin
    Routine "dump_bi_entry" contains logic to decode rgw_bucket_dir_entry
    which can contain etags to be fixed.  However, it turns out this
    routine is never called, so let's just get rid of it.  (The replacement
    code lives in cls land.)
    
    XXX temp proof of concept - should really remove it not #if 0
    
    Fixes: https://tracker.ceph.com/issues/?????
    
    Signed-off-by: Marcus Watts <mwatts@redhat.com>
    Resolves: rhbz#1995365

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -1129,6 +1129,8 @@ log_type get_log_type(const string& type_str) {
   return static_cast<log_type>(0xff);
 }
 
+#if 0
+// XXX not used?  remove me.
 void dump_bi_entry(bufferlist& bl, BIIndexType index_type, Formatter *formatter)
 {
   auto iter = bl.cbegin();
@@ -1138,6 +1140,7 @@ void dump_bi_entry(bufferlist& bl, BIIndexType index_type, Formatter *formatter)
       {
         rgw_bucket_dir_entry entry;
         decode(entry, iter);
+        rgw_fix_etag(xxx, entry.meta.etag);
         encode_json("entry", entry, formatter);
       }
       break;
@@ -1153,6 +1156,7 @@ void dump_bi_entry(bufferlist& bl, BIIndexType index_type, Formatter *formatter)
       break;
   }
 }
+#endif
 
 static void show_user_info(RGWUserInfo& info, Formatter *formatter)
 {
