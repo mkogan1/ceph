c5c7e4d76fac77299b329a282df8d3db4583f77b
myaoo18 <mandy118@bu.edu>
2021-12-13 14:43:56 +0000

    update directory unit test to include file size matching number of blocks

--- src/rgw_directory_unitTest.cc
+++ src/rgw_directory_unitTest.cc
@@ -10,49 +10,47 @@ using namespace std;
 int main () {
     ifstream rgwKeys, metaKeys;
     string line1, line2;
-    int lineNum = 0;
-    bool match = true;
 
     rgwKeys.open ("rgwKeys.txt");
     metaKeys.open ("metaKeys.txt");
 
+    int numOfBlocks = 1;
+    bool match = true;
+
     if (rgwKeys.is_open() && metaKeys.is_open()) {
 
+        /* Check to see if first line has .dat format to save the file size */
         rgwKeys >> line1;
         if(line1.substr(line1.length()-3) != "dat") {
             cout << "Fail: Object file did not get saved into meta data. First line of .dat is missing." << endl;
-            exit(0);
+            exit(1);
         }
-        // int size = stoi(line1.substr(line1.find("_")+1));
-
-        // Checks for EOF of either rgwKeys.txt or metaKeys.txt
-        // because we know rgwKeys.txt will have the .dat file at the end,
-        // which we will compare to the object name (TODO)
-        while (!rgwKeys.eof() && !metaKeys.eof()) {
-            lineNum++;
-
-            rgwKeys >> line1;
-            metaKeys >> line2;
-
+        int size = stoi(line1.substr(line1.find("_")+1));
+        
+        /* Check each line in metaKeys and rgwKeys to see if they match. Both files were sorted in order. */
+        while (rgwKeys >> line1 && metaKeys >> line2) {
             if (line1 != line2) {
-                cout << "Fail: The keys do not match on line " << lineNum << "." << endl;
+                cout << "Fail: The keys do not match on line " << numOfBlocks << "." << endl;
                 match = false;
-                break;
+                exit(1);
             }
+            numOfBlocks++;
         }
 
         if (match)
             cout << "Success: The keys match." << endl;
-        // Checking the object size matches the number of blocks 
-        // if (ceil(size/(double)4) == numOfBlocks) 
-        //     cout << "Success! Object size of " << size << " makes " << numOfBlocks << " blocks." << endl;
-        // else cout << "Fail! Object size does not match number of blocks" << endl;
+    
+       /* Check the object size if it matches the number of blocks */
+        if (ceil(size/(double)4) == numOfBlocks) 
+            cout << "Success! Object size of " << size << " makes " << numOfBlocks << " blocks." << endl;
+        else cout << "Fail! Object size does not match number of blocks" << endl;
 
         rgwKeys.close();
         metaKeys.close();
     }
     else
         cout << "Fail: Unable to open files." << endl;
+        exit(2);
 
     return 0;
 }
