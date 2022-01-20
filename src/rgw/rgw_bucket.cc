92dc88698e0346d839b7dbb2544a65be6289e06b
J. Eric Ivancich <ivancich@redhat.com>
2022-01-20 10:57:32 -0500

    rgw: make bucket index pending op expiration configureable
    Bucket index operations are transactional with data object
    manipulation. The operation is prepared by adding a pending operation
    record. And when the data object side is complete, the bucket index
    operation is committed.
    
    If it fails to be committed, later bucket listings will compare the
    pending ops with the current data object state and see whether it
    completed or not and then either commit or expire the op. The time
    span for expiration is currently hard-coded as 120 seconds (unless
    overridden in the bucket header, which can happen during "bucket
    check").
    
    This commit allows that expiration time to be configured.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>
    
    Resolves: rhbz#2042585

--- src/rgw/rgw_bucket.cc
+++ src/rgw/rgw_bucket.cc
@@ -44,7 +44,8 @@
 #define dout_context g_ceph_context
 #define dout_subsys ceph_subsys_rgw
 
-#define BUCKET_TAG_TIMEOUT 30
+// seconds for timeout during RGWBucket::check_object_index
+constexpr uint64_t BUCKET_TAG_QUICK_TIMEOUT = 30;
 
 // default number of entries to list with each bucket listing call
 // (use marker to bridge between calls)
@@ -1475,7 +1476,8 @@ int RGWBucket::check_object_index(RGWBucketAdminOpState& op_state,
     return -EINVAL;
   }
 
-  store->cls_obj_set_bucket_tag_timeout(bucket_info, BUCKET_TAG_TIMEOUT);
+  // use a quicker/shorter tag timeout during this process
+  store->cls_obj_set_bucket_tag_timeout(bucket_info, BUCKET_TAG_QUICK_TIMEOUT);
 
   string prefix;
   rgw_obj_index_key marker;
@@ -1515,6 +1517,7 @@ int RGWBucket::check_object_index(RGWBucketAdminOpState& op_state,
 
   formatter->close_section();
 
+  // restore normal tag timeout for bucket
   store->cls_obj_set_bucket_tag_timeout(bucket_info, 0);
 
   return 0;
