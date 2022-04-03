0eae89546c529b182fd7d9ac541e6db6b67f2224
Matt Benjamin <mbenjamin@redhat.com>
2022-04-03 16:36:22 -0400

    rgwlc:  don't incorrectly expire delete markers when !next_key_name
    This is a fencepost case when operating on the last element of a
    listing, apparently masked before Mark Kogan's segfault fix).
    
    Fixes: https://tracker.ceph.com/issues/55168
    Resolves: rhbz#2071137
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 9cfc37807971b491176c1e8060e04d253071341e)

--- src/rgw/rgw_lc.cc
+++ src/rgw/rgw_lc.cc
@@ -1023,20 +1023,22 @@ public:
       return false;
     }
     if (o.is_delete_marker()) {
-      std::string nkn;
-      if (oc.next_key_name) nkn = *oc.next_key_name;
-      if (oc.next_has_same_name(o.key.name)) {
-	ldout(oc.cct, 7) << __func__ << "(): dm-check SAME: key=" << o.key
-		       << " next_key_name: %%" << nkn << "%% "
-		       << oc.wq->thr_name() << dendl;
-	return false;
-      } else {
-	ldout(oc.cct, 7) << __func__ << "(): dm-check DELE: key=" << o.key
-			 << " next_key_name: %%" << nkn << "%% "
-			 << oc.wq->thr_name() << dendl;
+      if (oc.next_key_name) {
+	std::string nkn = *oc.next_key_name;
+	if (oc.next_has_same_name(o.key.name)) {
+	  ldout(oc.cct, 7) << __func__ << "(): dm-check SAME: key=" << o.key
+			   << " next_key_name: %%" << nkn << "%% "
+			   << oc.wq->thr_name() << dendl;
+	  return false;
+	} else {
+	  ldout(oc.cct, 7) << __func__ << "(): dm-check DELE: key=" << o.key
+			   << " next_key_name: %%" << nkn << "%% "
+			   << oc.wq->thr_name() << dendl;
         *exp_time = real_clock::now();
         return true;
+	}
       }
+      return false;
     }
 
     auto& mtime = o.meta.mtime;
