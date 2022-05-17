sunilangadi2/ceph-ci

rgw: permit logging of list-bucket (and any other no-bucket op)
This limitation is from inception (2011), but just recently reported by
a site using ops-log for system audit.

Fixes: https://tracker.ceph.com/issues/49313

Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

src/rgw/rgw_log.cc
@@ -341,32 +341,33 @@ int rgw_log_op(rgw::sal::Store* store, RGWREST* const rest, struct req_state *s,
     return 0;
 
   if (s->bucket_name.empty()) {
-    ldout(s->cct, 5) << "nothing to log for operation" << dendl;
-    return -EINVAL;
-  }
-  if (s->err.ret == -ERR_NO_SUCH_BUCKET || rgw::sal::Bucket::empty(s->bucket.get())) {
-    if (!s->cct->_conf->rgw_log_nonexistent_bucket) {
-      ldout(s->cct, 5) << "bucket " << s->bucket_name << " doesn't exist, not logging" << dendl;
+    /* this case is needed for, e.g., list_buckets */
+  } else {
+    if (s->err.ret == -ERR_NO_SUCH_BUCKET ||
+	rgw::sal::Bucket::empty(s->bucket.get())) {
+      if (!s->cct->_conf->rgw_log_nonexistent_bucket) {
+	ldout(s->cct, 5) << "bucket " << s->bucket_name << " doesn't exist, not logging" << dendl;
+	return 0;
+      }
+      bucket_id = "";
+    } else {
+      bucket_id = s->bucket->get_bucket_id();
+    }
+    entry.bucket = rgw_make_bucket_entry_name(s->bucket_tenant, s->bucket_name);
+
+    if (check_utf8(entry.bucket.c_str(), entry.bucket.size()) != 0) {
+      ldout(s->cct, 5) << "not logging op on bucket with non-utf8 name" << dendl;
       return 0;
     }
-    bucket_id = "";
-  } else {
-    bucket_id = s->bucket->get_bucket_id();
-  }
-  entry.bucket = rgw_make_bucket_entry_name(s->bucket_tenant, s->bucket_name);
 
-  if (check_utf8(entry.bucket.c_str(), entry.bucket.size()) != 0) {
-    ldout(s->cct, 5) << "not logging op on bucket with non-utf8 name" << dendl;
-    return 0;
-  }
+    if (!rgw::sal::Object::empty(s->object.get())) {
+      entry.obj = s->object->get_key();
+    } else {
+      entry.obj = rgw_obj_key("-");
+    }
 
-  if (!rgw::sal::Object::empty(s->object.get())) {
-    entry.obj = s->object->get_key();
-  } else {
-    entry.obj = rgw_obj_key("-");
-  }
-
-  entry.obj_size = s->obj_size;
+    entry.obj_size = s->obj_size;
+  } /* !bucket empty */
 
   if (s->cct->_conf->rgw_remote_addr_param.length())
     set_param_str(s, s->cct->_conf->rgw_remote_addr_param.c_str(),

mkogan1/ceph

Resolves: rhbz#1929387
(cherry picked from commit 88b08945ed3537010dcc7c917faca7d23ade9df1)
(cherry picked from commit abe3a89d5d5b4e32faacbc33bc7e3182be3e4c5c)
Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/rgw/rgw_log.cc
@@ -518,32 +518,33 @@ int rgw_log_op(RGWREST* const rest, struct req_state *s, const string& op_name, 
     return 0;
 
   if (s->bucket_name.empty()) {
-    ldpp_dout(s, 5) << "nothing to log for operation" << dendl;
-    return -EINVAL;
-  }
-  if (s->err.ret == -ERR_NO_SUCH_BUCKET || rgw::sal::RGWBucket::empty(s->bucket.get())) {
-    if (!s->cct->_conf->rgw_log_nonexistent_bucket) {
-      ldpp_dout(s, 5) << "bucket " << s->bucket_name << " doesn't exist, not logging" << dendl;
+    /* this case is needed for, e.g., list_buckets */
+  } else {
+    if (s->err.ret == -ERR_NO_SUCH_BUCKET ||
+	rgw::sal::RGWBucket::empty(s->bucket.get())) {
+      if (!s->cct->_conf->rgw_log_nonexistent_bucket) {
+	ldpp_dout(s, 5) << "bucket " << s->bucket_name << " doesn't exist, not logging" << dendl;
+	return 0;
+      }
+      bucket_id = "";
+    } else {
+      bucket_id = s->bucket->get_bucket_id();
+    }
+    entry.bucket = rgw_make_bucket_entry_name(s->bucket_tenant, s->bucket_name);
+
+    if (check_utf8(entry.bucket.c_str(), entry.bucket.size()) != 0) {
+      ldpp_dout(s, 5) << "not logging op on bucket with non-utf8 name" << dendl;
       return 0;
     }
-    bucket_id = "";
-  } else {
-    bucket_id = s->bucket->get_bucket_id();
-  }
-  entry.bucket = rgw_make_bucket_entry_name(s->bucket_tenant, s->bucket_name);
 
-  if (check_utf8(entry.bucket.c_str(), entry.bucket.size()) != 0) {
-    ldpp_dout(s, 5) << "not logging op on bucket with non-utf8 name" << dendl;
-    return 0;
-  }
+    if (!rgw::sal::RGWObject::empty(s->object.get())) {
+      entry.obj = s->object->get_key();
+    } else {
+      entry.obj = rgw_obj_key("-");
+    }
 
-  if (!rgw::sal::RGWObject::empty(s->object.get())) {
-    entry.obj = s->object->get_key();
-  } else {
-    entry.obj = rgw_obj_key("-");
-  }
-
-  entry.obj_size = s->obj_size;
+    entry.obj_size = s->obj_size;
+  } /* !bucket empty */
 
   if (s->cct->_conf->rgw_remote_addr_param.length())
     set_param_str(s, s->cct->_conf->rgw_remote_addr_param.c_str(),