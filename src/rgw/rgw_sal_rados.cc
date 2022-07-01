1a7cbb6853d7b8743df2d7be8d8f565ed45b068a
Matt Benjamin <mbenjamin@redhat.com>
2022-07-01 17:55:33 -0400

    rgwlc:  zone-conditional lifecycle processing
    Lifecycle rules with the ArchiveZone flag must execute on archive zones,
    but must not execute on others.
    
    Fixes: https://tracker.ceph.com/issues/56440
    Resolves: rhbz#2095670
    
    conflicts:
      rgw_lc.cc
      rgw_sal_rados.h
      rgw_sal_rados.cc
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 98c598bde777d8cc1892a99287f9e22cd211a8e8)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_sal_rados.cc
+++ src/rgw/rgw_sal_rados.cc
@@ -2732,6 +2732,11 @@ const rgw_zone_id& RadosZone::get_id()
   return store->svc()->zone->zone_id();
 }
 
+const std::string_view RadosZone::get_tier_type()
+{
+  return store->svc()->zone->get_zone().tier_type;
+}
+
 const RGWRealm& RadosZone::get_realm()
 {
   return store->svc()->zone->get_realm();
@@ -3176,5 +3181,4 @@ void* newStore(void)
 
   return store;
 }
-
 }
