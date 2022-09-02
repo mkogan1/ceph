scantist-ossops-m2/ceph-ci

rgw: RGWContinuousLeaseCR takes arguments by value and moves them
Since we were taking them by reference and copying before, this is
strictly better. Callers that give us an RValue can skip the copy.

Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/rgw/rgw_cr_rados.h
@@ -1403,31 +1403,30 @@ public:
 };
 
 class RGWContinuousLeaseCR : public RGWCoroutine {
-  RGWAsyncRadosProcessor *async_rados;
+  RGWAsyncRadosProcessor* async_rados;
   rgw::sal::RadosStore* store;
 
   const rgw_raw_obj obj;
 
   const std::string lock_name;
-  const std::string cookie;
+  const std::string cookie{RGWSimpleRadosLockCR::gen_random_cookie(cct)};
 
   int interval;
-  bool going_down{ false };
+  bool going_down{false};
   bool locked{false};
 
-  RGWCoroutine *caller;
+  RGWCoroutine* caller;
 
   bool aborted{false};
 
 public:
-  RGWContinuousLeaseCR(RGWAsyncRadosProcessor *_async_rados, rgw::sal::RadosStore* _store,
-                       const rgw_raw_obj& _obj,
-                       const std::string& _lock_name, int _interval, RGWCoroutine *_caller)
-    : RGWCoroutine(_store->ctx()), async_rados(_async_rados), store(_store),
-    obj(_obj), lock_name(_lock_name),
-    cookie(RGWSimpleRadosLockCR::gen_random_cookie(cct)),
-    interval(_interval), caller(_caller)
-  {}
+  RGWContinuousLeaseCR(RGWAsyncRadosProcessor* async_rados,
+		       rgw::sal::RadosStore* _store,
+                       rgw_raw_obj obj, std::string lock_name,
+		       int interval, RGWCoroutine* caller)
+    : RGWCoroutine(_store->ctx()), async_rados(async_rados), store(_store),
+      obj(std::move(obj)), lock_name(std::move(lock_name)),
+      interval(interval), caller(caller) {}
 
   virtual ~RGWContinuousLeaseCR() override;
 

mkogan1/ceph

(cherry picked from commit 7f6a6b190d4a31993430fe4bbbd0c1e0f0c5bab7)
Conflicts:
	src/rgw/rgw_cr_rados.h
 - Upstream SAL
Resolves: rhbz#2105324

src/rgw/rgw_cr_rados.h
@@ -1427,17 +1427,17 @@ class RGWContinuousLeaseCR : public RGWCoroutine {
 
   const rgw_raw_obj obj;
 
-  const string lock_name;
-  const string cookie;
+  const std::string lock_name;
+  const std::string cookie{RGWSimpleRadosLockCR::gen_random_cookie(cct)};
 
   int interval;
-  bool going_down{ false };
+  bool going_down{false};
   bool locked{false};
   
-  const ceph::timespan interval_tolerance;
-  const ceph::timespan ts_interval;
+  const ceph::timespan interval_tolerance = ceph::make_timespan(9*interval/10);
+  const ceph::timespan ts_interval = ceph::make_timespan(interval);
 
-  RGWCoroutine *caller;
+  RGWCoroutine* caller;
 
   bool aborted{false};
   
@@ -1445,15 +1445,13 @@ class RGWContinuousLeaseCR : public RGWCoroutine {
   ceph::coarse_mono_time current_time;
 
 public:
-  RGWContinuousLeaseCR(RGWAsyncRadosProcessor *_async_rados, rgw::sal::RGWRadosStore *_store,
-                       const rgw_raw_obj& _obj,
-                       const string& _lock_name, int _interval, RGWCoroutine *_caller)
-    : RGWCoroutine(_store->ctx()), async_rados(_async_rados), store(_store),
-    obj(_obj), lock_name(_lock_name),
-    cookie(RGWSimpleRadosLockCR::gen_random_cookie(cct)),
-    interval(_interval), interval_tolerance(ceph::make_timespan(9*interval/10)), ts_interval(ceph::make_timespan(interval)),
-      caller(_caller)
-  {}
+  RGWContinuousLeaseCR(RGWAsyncRadosProcessor* async_rados,
+		       rgw::sal::RGWRadosStore* _store,
+                       rgw_raw_obj obj, std::string lock_name,
+		       int interval, RGWCoroutine* caller)
+    : RGWCoroutine(_store->ctx()), async_rados(async_rados), store(_store),
+      obj(std::move(obj)), lock_name(std::move(lock_name)),
+      interval(interval), caller(caller) {}
 
   int operate(const DoutPrefixProvider *dpp) override;
 