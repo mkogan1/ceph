scantist-ossops-m2/ceph-ci

mgr/cephadm: Adding --storage.tsdb.retention.size prometheus option fixes: https://tracker.ceph.com/issues/57422
Signed-off-by: Redouane Kachach <rkachach@redhat.com>

src/pybind/mgr/cephadm/services/monitoring.py
@@ -298,6 +298,12 @@ class PrometheusService(CephadmService):
         except AttributeError:
             retention_time = '15d'
 
+        try:
+            retention_size = spec.retention_size if spec.retention_size else '0'
+        except AttributeError:
+            # default to disabled
+            retention_size = '0'
+
         t = self.mgr.get('mgr_map').get('services', {}).get('prometheus', None)
         sd_port = self.mgr.service_discovery_port
         srv_end_point = ''
@@ -332,7 +338,8 @@ class PrometheusService(CephadmService):
                 'prometheus.yml': self.mgr.template.render('services/prometheus/prometheus.yml.j2', context),
                 'root_cert.pem': self.mgr.http_server.service_discovery.ssl_certs.get_root_cert()
             },
-            'retention_time': retention_time
+            'retention_time': retention_time,
+            'retention_size': retention_size
         }
 
         # include alerts, if present in the container

mkogan1/ceph

(cherry picked from commit 4da92c59597dcbf0bba4be50db73233e34108ca9)
Conflicts:
	src/pybind/mgr/cephadm/services/monitoring.py
	src/pybind/mgr/cephadm/tests/test_services.py
(cherry picked from commit 8b72ce266c0666b4da66977164416825e49d99dc)
	src/pybind/mgr/cephadm/module.py
	src/pybind/mgr/cephadm/tests/test_cephadm.py
Resolves: rhbz#2112800

src/pybind/mgr/cephadm/services/monitoring.py
@@ -266,6 +266,12 @@ class PrometheusService(CephadmService):
         except AttributeError:
             retention_time = '15d'
 
+        try:
+            retention_size = prom_spec.retention_size if prom_spec.retention_size else '0'
+        except AttributeError:
+            # default to disabled
+            retention_size = '0'
+
         deps = []  # type: List[str]
 
         # scrape mgrs
@@ -346,7 +352,8 @@ class PrometheusService(CephadmService):
                     self.mgr.template.render(
                         'services/prometheus/prometheus.yml.j2', context)
             },
-            'retention_time': retention_time
+            'retention_time': retention_time,
+            'retention_size': retention_size
         }
 
         # include alerts, if present in the container