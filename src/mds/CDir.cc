14e301caf4648db043c323dd9b61b5fe4324cfd7
Neeraj Pratap Singh <neesingh@redhat.com>
2022-11-17 23:33:15 +0530

    mds: scrub repair does not clear earlier damage health status
    Resolves: rhbz#2256581
    Fixes: https://tracker.ceph.com/issues/54557
    Signed-off-by: Neeraj Pratap Singh <neesingh@redhat.com>
    (cherry picked from commit 5b56098f17dd9abe4c15cbc7f487c0e94841beaf)
    (cherry picked from commit 8ae8fc69c91b8521622f9ba5b7caf9376177a335)

--- src/mds/CDir.cc
+++ src/mds/CDir.cc
@@ -3759,6 +3759,7 @@ bool CDir::scrub_local()
     mdcache->repair_dirfrag_stats(this);
     scrub_infop->header->set_repaired();
     good = true;
+    mdcache->mds->damage_table.remove_dentry_damage_entry(this);
   }
   return good;
 }
