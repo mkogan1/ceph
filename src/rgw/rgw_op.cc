681190066b6bea6d3dde8f299978f940b8588c0f
Tongliang Deng <dengtongliang@gmail.com>
2022-12-13 06:42:34 +0000

    rgw/sse-s3: fix bucket encryption of multipart upload
    Multipart upload missing encryption when we have bucket encryption
    policy. Fix it by fetching bucket encryption policy and resolving
    defaults at multipart init op.
    
    Fixes: https://tracker.ceph.com/issues/59218
    
    Signed-off-by: Tongliang Deng <dengtongliang@gmail.com>
    (cherry picked from commit 6d9e4f7924c6149d23919ef82bc09406e1290164)
    
    Conflicts:
    	src/rgw/rgw_rest_s3.cc
    Resolves: rhbz#2214252

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -6228,8 +6228,10 @@ void RGWInitMultipart::execute(optional_yield y)
   bufferlist aclbl;
   rgw::sal::RGWAttrs attrs;
 
-  if (get_params(y) < 0)
+  op_ret = get_params(y);
+  if (op_ret < 0) {
     return;
+  }
 
   if (rgw::sal::RGWObject::empty(s->object.get()))
     return;
