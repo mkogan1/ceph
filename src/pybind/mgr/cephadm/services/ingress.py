anshuman-agarwala/ceph-ci

mgr/cephadm: write out haproxy.key is ssl_key field provided in ingress spec
If you put the cert into the ssl_cert field and key into the
ssl_key field, currently we do nothing with the ssl_key. This results
in the haproxy daemon failing to startup with something like

[ALERT] 205/180924 (2) : parsing [/var/lib/haproxy/haproxy.cfg:40] : 'bind 192.168.122.100:8090' : No Private Key found in '/var/lib/haproxy/haproxy.pem.key
[ALERT] 205/180924 (2) : Error(s) found in configuration file : /var/lib/haproxy/haproxy.cfg
[ALERT] 205/180924 (2) : Fatal errors found in configuration.

Signed-off-by: Adam King <adking@redhat.com>

src/pybind/mgr/cephadm/services/ingress.py
@@ -197,9 +197,10 @@ class IngressService(CephService):
         }
         if spec.ssl_cert:
             ssl_cert = spec.ssl_cert
-            if isinstance(ssl_cert, list):
-                ssl_cert = '\n'.join(ssl_cert)
             config_files['files']['haproxy.pem'] = ssl_cert
+        if spec.ssl_key:
+            ssl_key = spec.ssl_key
+            config_files['files']['haproxy.pem.key'] = ssl_key
 
         return config_files, sorted(deps)
 

mkogan1/ceph

(cherry picked from commit 56f96e49641342206f787f7e22b0ebac58885a3a)
Resolves: rhbz#2308344

src/pybind/mgr/cephadm/services/ingress.py
@@ -198,9 +198,10 @@ class IngressService(CephService):
         }
         if spec.ssl_cert:
             ssl_cert = spec.ssl_cert
-            if isinstance(ssl_cert, list):
-                ssl_cert = '\n'.join(ssl_cert)
             final_config['files']['haproxy.pem'] = ssl_cert
+        if spec.ssl_key:
+            ssl_key = spec.ssl_key
+            final_config['files']['haproxy.pem.key'] = ssl_key
 
         if spec.haproxy_qat_support:
             final_config['qat_support'] = True