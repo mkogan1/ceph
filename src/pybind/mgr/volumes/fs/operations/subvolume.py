d9261627ba52c6508e89183e81d9a240b781c2a0
Xavi Hernandez <xhernandez@gmail.com>
2025-03-04 17:05:44 +0100

    pybind/mgr: add option to set case sensitivity to a subvolume
    Resolves: rhbz#2355303
    
    Add an option to explicitly set the case sensitivity of a CephFS
    subvolume.
    
    Signed-off-by: Xavi Hernandez <xhernandez@gmail.com>
    (cherry picked from commit 87bb0896753add1da116f80c4a45c0a6233ea123)

--- src/pybind/mgr/volumes/fs/operations/subvolume.py
+++ src/pybind/mgr/volumes/fs/operations/subvolume.py
@@ -4,7 +4,7 @@ from .template import SubvolumeOpType
 
 from .versions import loaded_subvolumes
 
-def create_subvol(mgr, fs, vol_spec, group, subvolname, size, isolate_nspace, pool, mode, uid, gid, earmark, normalization):
+def create_subvol(mgr, fs, vol_spec, group, subvolname, size, isolate_nspace, pool, mode, uid, gid, earmark, normalization, case_insensitive):
     """
     create a subvolume (create a subvolume with the max known version).
 
@@ -19,10 +19,11 @@ def create_subvol(mgr, fs, vol_spec, group, subvolname, size, isolate_nspace, po
     :param gid: the group identifier
     :param earmark: metadata string to identify if subvolume is associated with nfs/smb
     :param normalization: the unicode normalization form to use (nfd, nfc, nfkd or nfkc)
+    :param case_insensitive: whether to make the subvolume case insensitive or not
     :return: None
     """
     subvolume = loaded_subvolumes.get_subvolume_object_max(mgr, fs, vol_spec, group, subvolname)
-    subvolume.create(size, isolate_nspace, pool, mode, uid, gid, earmark, normalization)
+    subvolume.create(size, isolate_nspace, pool, mode, uid, gid, earmark, normalization, case_insensitive)
 
 
 def create_clone(mgr, fs, vol_spec, group, subvolname, pool, source_volume, source_subvolume, snapname):
