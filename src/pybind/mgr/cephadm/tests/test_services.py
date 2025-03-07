5287913cdafe4d8455f8bf9361f4ef526319a411
Redouane Kachach <rkachach@ibm.com>
2025-03-07 06:54:24 +0100

    mgr/cephadm: use ip_hash to enforce the same prometheues upstream
    Implemented ip_hash in Nginx to ensure consistent upstream selection
    for Prometheus in HA scenarios. This helps maintain query consistency
    in Grafana and reduces gaps caused by Prometheus instance failovers.
    
    Fixes: https://tracker.ceph.com/issues/70352?
    
    Resolves: rhbz#2319330
    
    Signed-off-by: Redouane Kachach <rkachach@ibm.com>
    (cherry picked from commit bfcb5334c2f50fa32d8054d8800c6cede488f9d9)

--- src/pybind/mgr/cephadm/tests/test_services.py
+++ src/pybind/mgr/cephadm/tests/test_services.py
@@ -3922,6 +3922,7 @@ class TestMgmtGateway:
                                              }
 
                                              upstream prometheus_servers {
+                                              ip_hash;
                                               server 192.168.100.100:9095;
                                               server 192.168.100.101:9095;
                                              }
@@ -4173,6 +4174,7 @@ class TestMgmtGateway:
                                              }
 
                                              upstream prometheus_servers {
+                                              ip_hash;
                                               server 192.168.100.100:9095;
                                               server 192.168.100.101:9095;
                                              }
