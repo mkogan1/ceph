mkogan1/ceph

commit 6757b1ec8d969b8e9019f726890b7041f1a47795

tools/cephfs/DataScan: skip linkages that have been removed
Also: injected_inos may need to store multiple injected primary links found in
the metadata pool. This is especially true now that we may have multiple
primary links to a directory due to prior commit

    tools/cephfs/DataScan: create all ancestors during scan_inodes

Fixes: https://tracker.ceph.com/issues/63301
Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
(cherry picked from commit a66a68bfa5388e04afe3a8dcd58a08c3138e0b28)

Resolves: rhbz#2343973

src/tools/cephfs/DataScan.cc
@@ -935,10 +935,18 @@ int DataScan::scan_links()
     dirfrag_t dirfrag() const {
       return dirfrag_t(dirino, frag);
     }
+    void print(std::ostream& os) const {
+      os << "link_info_t(diri=" << dirino << "." << frag << " name=" << name << " v=" << version << " l=" << nlink << ")";
+    }
+    bool operator==(const link_info_t& o) const {
+      return dirino == o.dirino
+             && frag == o.frag
+             && name == o.name;
+    }
   };
   map<inodeno_t, list<link_info_t> > dup_primaries;
   map<inodeno_t, link_info_t> bad_nlink_inos;
-  map<inodeno_t, link_info_t> injected_inos;
+  multimap<inodeno_t, link_info_t> injected_inos;
 
   map<dirfrag_t, set<string> > to_remove;
 
@@ -1067,7 +1075,7 @@ int DataScan::scan_links()
 			     make_move_iterator(end(srnode.snaps)));
 	      }
 	      if (dnfirst == CEPH_NOSNAP) {
-                injected_inos[ino] = link_info_t(dir_ino, frag_id, dname, inode.inode);
+                injected_inos.insert({ino, link_info_t(dir_ino, frag_id, dname, inode.inode)});
                 dout(20) << "adding " << ino << " for future processing to fix dnfirst" << dendl;
               }
 	    }
@@ -1135,6 +1143,7 @@ int DataScan::scan_links()
 
     link_info_t newest;
     for (auto& q : p.second) {
+      dout(10) << " primary: " << q << dendl;
       if (q.version > newest.version) {
 	newest = q;
       } else if (q.version == newest.version &&
@@ -1143,6 +1152,7 @@ int DataScan::scan_links()
 	newest = q;
       }
     }
+    dout(10) << "newest is: " << newest << dendl;
 
     for (auto& q : p.second) {
       // in the middle of dir fragmentation?
@@ -1158,6 +1168,17 @@ int DataScan::scan_links()
       to_remove[q.dirfrag()].insert(key);
       derr << "Remove duplicated ino 0x" << p.first << " from "
 	   << q.dirfrag() << "/" << q.name << dendl;
+      {
+        /* we've removed the injected linkage: don't fix it later */
+        auto range = injected_inos.equal_range(p.first);
+        for (auto it = range.first; it != range.second; ) {
+          if (it->second == q) {
+            it = injected_inos.erase(it);
+          } else {
+            ++it;
+          }
+        }
+      }
     }
 
     int nlink = 0;
@@ -1172,6 +1193,8 @@ int DataScan::scan_links()
 	   << " has " << newest.nlink << dendl;
       bad_nlink_inos[p.first] = newest;
       bad_nlink_inos[p.first].nlink = nlink;
+    } else {
+      bad_nlink_inos.erase(p.first);
     }
   }
   dup_primaries.clear();

commit 4f4bc15c7cfc506b17c3d1f4e2ced7886a64733e

tools/DataScan: Fix operator<< overloading for C++11
This is required to fix the following build failure caused by
the commit 6757b1ec8d969b8e9019f726890b7041f1a47795

This is required to fix the following build failure caused by
the commit 6757b1ec8d969b8e9019f726890b7041f1a47795

...
/builddir/build/BUILD/ceph-17.2.6/src/tools/cephfs/DataScan.cc:1146:32: \
error: no match for 'operator<<' (operand types are 'std::basic_ostream<char>' \
and 'DataScan::scan_links()::link_info_t')
 1146 |       dout(10) << " primary: " << q << dendl;
...

The required dependant upstream commit 92df0fcc649345f438d0d7cc6fab658c02060f07
to fix the build error uses the 'concept' which is only available in C++20.
The 6.1 build uses C++11. So this patch worksaround it by defining the
'struct linkinfo_t' outside the 'DataScan::scan_links' to make operator<<
overload work.

Signed-off-by: Kotresh HR <khiremat@redhat.com>

Resolves: rhbz#2343973

src/tools/cephfs/DataScan.cc
@@ -906,6 +906,36 @@ bool DataScan::valid_ino(inodeno_t ino) const
     || ino == CEPH_INO_LOST_AND_FOUND;
 }
 
+struct link_info_t {
+  inodeno_t dirino;
+  frag_t frag;
+  string name;
+  version_t version;
+  int nlink;
+  bool is_dir;
+  map<snapid_t, SnapInfo> snaps;
+  link_info_t() : version(0), nlink(0), is_dir(false) {}
+  link_info_t(inodeno_t di, frag_t df, const string& n, const CInode::inode_const_ptr& i) :
+    dirino(di), frag(df), name(n),
+    version(i->version), nlink(i->nlink), is_dir(S_IFDIR & i->mode) {}
+  dirfrag_t dirfrag() const {
+    return dirfrag_t(dirino, frag);
+  }
+  void print(std::ostream& os) const {
+    os << "link_info_t(diri=" << dirino << "." << frag << " name=" << name << " v=" << version << " l=" << nlink << ")";
+  }
+  bool operator==(const link_info_t& o) const {
+    return dirino == o.dirino
+           && frag == o.frag
+           && name == o.name;
+  }
+};
+
+std::ostream& operator<<(std::ostream& os, const link_info_t& o) {
+  o.print(os);
+  return os;
+}
+
 int DataScan::scan_links()
 {
   MetadataDriver *metadata_driver = dynamic_cast<MetadataDriver*>(driver);
@@ -920,30 +950,6 @@ int DataScan::scan_links()
   snapid_t last_snap = 1;
   snapid_t snaprealm_v2_since = 2;
 
-  struct link_info_t {
-    inodeno_t dirino;
-    frag_t frag;
-    string name;
-    version_t version;
-    int nlink;
-    bool is_dir;
-    map<snapid_t, SnapInfo> snaps;
-    link_info_t() : version(0), nlink(0), is_dir(false) {}
-    link_info_t(inodeno_t di, frag_t df, const string& n, const CInode::inode_const_ptr& i) :
-      dirino(di), frag(df), name(n),
-      version(i->version), nlink(i->nlink), is_dir(S_IFDIR & i->mode) {}
-    dirfrag_t dirfrag() const {
-      return dirfrag_t(dirino, frag);
-    }
-    void print(std::ostream& os) const {
-      os << "link_info_t(diri=" << dirino << "." << frag << " name=" << name << " v=" << version << " l=" << nlink << ")";
-    }
-    bool operator==(const link_info_t& o) const {
-      return dirino == o.dirino
-             && frag == o.frag
-             && name == o.name;
-    }
-  };
   map<inodeno_t, list<link_info_t> > dup_primaries;
   map<inodeno_t, link_info_t> bad_nlink_inos;
   multimap<inodeno_t, link_info_t> injected_inos;