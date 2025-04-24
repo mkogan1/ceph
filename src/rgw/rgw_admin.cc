mkogan1/ceph

commit 790c38eacc52cc4c14beb48fca8b204235632793

rgw/logging: add support for GetBucketLogging and PutBucketLogging
this is based on AWS server access logs:
- https://docs.aws.amazon.com/AmazonS3/latest/userguide/enable-server-access-logging.html
- https://docs.aws.amazon.com/AmazonS3/latest/userguide/ServerLogs.html
- https://docs.aws.amazon.com/AmazonS3/latest/API/API_PutBucketLogging.html
- https://docs.aws.amazon.com/AmazonS3/latest/API/API_GetBucketLogging.html

however, a new mode was added called "journal" where:
- logs of PUT, COPY and MPU are guaranteed
- we have logs of DELETE and multi-DELETE operations (not guaranteed)
- log records hold only minimal amount of information

Fixes: https://tracker.ceph.com/issues/984

Signed-off-by: Yuval Lifshitz <ylifshit@redhat.com>

src/rgw/rgw_admin.cc
@@ -68,6 +68,7 @@ extern "C" {
 #include "rgw_sal_config.h"
 #include "rgw_data_access.h"
 #include "rgw_account.h"
+#include "rgw_bucket_logging.h"
 
 #include "services/svc_sync_modules.h"
 #include "services/svc_cls.h"
@@ -177,6 +178,7 @@ void usage()
   cout << "  bucket sync disable              disable bucket sync\n";
   cout << "  bucket sync enable               enable bucket sync\n";
   cout << "  bucket radoslist                 list rados objects backing bucket's objects\n";
+  cout << "  bucket logging flush             flush pending log records object of source bucket to the log bucket to bucket\n";
   cout << "  bi get                           retrieve bucket index object entries\n";
   cout << "  bi put                           store bucket index object entries\n";
   cout << "  bi list                          list raw bucket index entries\n";
@@ -704,6 +706,7 @@ enum class OPT {
   BUCKET_SHARD_OBJECTS,
   BUCKET_OBJECT_SHARD,
   BUCKET_RESYNC_ENCRYPTED_MULTIPART,
+  BUCKET_LOGGING_FLUSH,
   POLICY,
   LOG_LIST,
   LOG_SHOW,
@@ -942,6 +945,7 @@ static SimpleCmd::Commands all_cmds = {
   { "bucket shard object", OPT::BUCKET_SHARD_OBJECTS },
   { "bucket object shard", OPT::BUCKET_OBJECT_SHARD },
   { "bucket resync encrypted multipart", OPT::BUCKET_RESYNC_ENCRYPTED_MULTIPART },
+  { "bucket logging flush", OPT::BUCKET_LOGGING_FLUSH },
   { "policy", OPT::POLICY },
   { "log list", OPT::LOG_LIST },
   { "log show", OPT::LOG_SHOW },
@@ -7550,6 +7554,54 @@ int main(int argc, const char **argv)
       cerr << "failure: " << cpp_strerror(-r) << ": " << err << std::endl;
       return -r;
     }
+  }
+
+  if (opt_cmd == OPT::BUCKET_LOGGING_FLUSH) {
+    if (bucket_name.empty()) {
+      cerr << "ERROR: bucket not specified" << std::endl;
+      return EINVAL;
+    }
+    int ret = init_bucket(tenant, bucket_name, bucket_id, &bucket);
+    if (ret < 0) {
+      return -ret;
+    }
+    const auto& bucket_attrs = bucket->get_attrs();
+    auto iter = bucket_attrs.find(RGW_ATTR_BUCKET_LOGGING);
+    if (iter == bucket_attrs.end()) {
+      cerr << "WARNING: no logging configured on bucket" << std::endl;
+      return 0;
+    }
+    rgw::bucketlogging::configuration configuration;
+    try {
+      configuration.enabled = true;
+      decode(configuration, iter->second);
+    } catch (buffer::error& err) {
+      cerr << "ERROR: failed to decode logging attribute '" << RGW_ATTR_BUCKET_LOGGING
+        << "'. error: " << err.what() << std::endl;
+      return  EINVAL;
+    }
+    std::unique_ptr<rgw::sal::Bucket> target_bucket;
+    ret = init_bucket(tenant, configuration.target_bucket, "", &target_bucket);
+    if (ret < 0) {
+      cerr << "ERROR: failed to get target logging bucket '" << configuration.target_bucket << "'" << std::endl;
+      return -ret;
+    }
+    std::string obj_name;
+    RGWObjVersionTracker objv_tracker;
+    ret = target_bucket->get_logging_object_name(obj_name, configuration.target_prefix, null_yield, dpp(), &objv_tracker);
+    if (ret < 0) {
+      cerr << "ERROR: failed to get pending logging object name from target bucket '" << configuration.target_bucket << "'" << std::endl;
+      return -ret;
+    }
+    ret = rgw::bucketlogging::rollover_logging_object(configuration, target_bucket, obj_name, dpp(), null_yield, true, &objv_tracker);
+    if (ret < 0) {
+      cerr << "ERROR: failed to flush pending logging object '" << obj_name
+        << "' to target bucket '" << configuration.target_bucket << "'" << std::endl;
+      return -ret;
+    }
+    cerr << "flushed pending logging object '" << obj_name
+      << "' to target bucket '" << configuration.target_bucket << "'" << std::endl;
+    return 0;
   }
 
   if (opt_cmd == OPT::LOG_LIST) {

src/rgw/rgw_bucket_logging.h
@@ -1,0 +1,173 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab ft=cpp
+
+#pragma once
+
+#include <string>
+#include <optional>
+#include <cstdint>
+#include "rgw_sal_fwd.h"
+#include "include/buffer.h"
+#include "include/encoding.h"
+#include "common/async/yield_context.h"
+
+class XMLObj;
+namespace ceph { class Formatter; }
+class DoutPrefixProvider;
+struct req_state;
+class RGWObjVersionTracker;
+class RGWOp;
+
+namespace rgw::bucketlogging {
+/* S3 bucket logging configuration
+ * based on: https://docs.aws.amazon.com/AmazonS3/latest/API/API_PutBucketLogging.html
+ * with ceph extensions
+<BucketLoggingStatus xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+   <LoggingEnabled>
+      <TargetBucket>string</TargetBucket>
+      <TargetGrants>
+         <Grant>
+            <Grantee>
+               <DisplayName>string</DisplayName>
+               <EmailAddress>string</EmailAddress>
+               <ID>string</ID>
+               <xsi:type>string</xsi:type>
+               <URI>string</URI>
+            </Grantee>
+            <Permission>string</Permission>
+         </Grant>
+      </TargetGrants>
+      <TargetObjectKeyFormat>
+         <PartitionedPrefix>
+            <PartitionDateSource>DeliveryTime|EventTime</PartitionDateSource>
+         </PartitionedPrefix>
+         <SimplePrefix>
+         </SimplePrefix>
+      </TargetObjectKeyFormat>
+      <TargetPrefix>string</TargetPrefix>
+      <LoggingType>Standard|Journal</LoggingType>       <!-- Ceph extension -->
+      <ObjectRollTime>integer</ObjectRollTime>          <!-- Ceph extension -->
+      <RecordsBatchSize>integer</RecordsBatchSize>      <!-- Ceph extension -->
+   </LoggingEnabled>
+</BucketLoggingStatus>
+*/
+
+enum class KeyFormat {Partitioned, Simple};
+enum class LoggingType {Standard, Journal, Any};
+enum class PartitionDateSource {DeliveryTime, EventTime};
+
+struct configuration {
+  uint32_t default_obj_roll_time = 300;
+  bool enabled = false;
+  std::string target_bucket;
+  KeyFormat obj_key_format = KeyFormat::Simple;
+  // target object key formats:
+  // Partitioned: [DestinationPrefix][SourceAccountId]/[SourceRegion]/[SourceBucket]/[YYYY]/[MM]/[DD]/[YYYY]-[MM]-[DD]-[hh]-[mm]-[ss]-[UniqueString]
+  // Simple: [DestinationPrefix][YYYY]-[MM]-[DD]-[hh]-[mm]-[ss]-[UniqueString]
+  std::string target_prefix; // a prefix for all log object keys. 
+                             // useful when multiple bucket log to the same target 
+                             // or when the target bucket is used for other things than logs
+  uint32_t obj_roll_time; // time in seconds to move object to bucket and start another object
+  LoggingType logging_type = LoggingType::Standard;
+  // in case of "Standard: logging type, all bucket operations are logged
+  // in case of "Journal" logging type only the following operations are logged: PUT, COPY, MULTI/DELETE, Complete MPU
+  uint32_t records_batch_size = 0; // how many records to batch in memory before writing to the object
+                                   // if set to zero, records are written syncronously to the object.
+                                   // if obj_roll_time is reached, the batch of records will be written to the object
+                                   // regardless of the number of records
+  PartitionDateSource date_source = PartitionDateSource::DeliveryTime;
+  // EventTime: use only year, month, and day. The hour, minutes and seconds are set to 00 in the key
+  // DeliveryTime: the time the log object was created
+  bool decode_xml(XMLObj *obj);
+  void dump_xml(Formatter *f) const;
+  void dump(Formatter *f) const; // json
+  std::string to_json_str() const;
+
+  void encode(ceph::bufferlist& bl) const {
+    ENCODE_START(1, 1, bl);
+    encode(target_bucket, bl);
+    encode(static_cast<int>(obj_key_format), bl);
+    encode(target_prefix, bl);
+    encode(obj_roll_time, bl);
+    encode(static_cast<int>(logging_type), bl);
+    encode(records_batch_size, bl);
+    encode(static_cast<int>(date_source), bl);
+    ENCODE_FINISH(bl);
+  }
+
+  void decode(bufferlist::const_iterator& bl) {
+    DECODE_START(1, bl);
+    decode(target_bucket, bl);
+    int type;
+    decode(type, bl);
+    obj_key_format = static_cast<KeyFormat>(type);
+    decode(target_prefix, bl);
+    decode(obj_roll_time, bl);
+    decode(type, bl);
+    logging_type = static_cast<LoggingType>(type);
+    decode(records_batch_size, bl);
+    decode(type, bl);
+    date_source = static_cast<PartitionDateSource>(type);
+    DECODE_FINISH(bl);
+  }
+};
+WRITE_CLASS_ENCODER(configuration)
+
+constexpr unsigned MAX_BUCKET_LOGGING_BUFFER = 1000;
+
+using bucket_logging_records = std::array<std::string, MAX_BUCKET_LOGGING_BUFFER>;
+
+template <typename Records>
+inline std::string to_string(const Records& records) {
+  std::string str_records;
+  for (const auto& record : records) {
+    str_records.append(to_string(record)).append("\n");
+  }
+  return str_records;
+}
+
+// log a bucket logging record according to the configuration
+int log_record(rgw::sal::Driver* driver,
+    const sal::Object* obj,
+    const req_state* s, 
+    const std::string& op_name, 
+    const std::string& etag, 
+    size_t size, 
+    const configuration& conf,
+    const DoutPrefixProvider *dpp, 
+    optional_yield y, 
+    bool async_completion,
+    bool log_source_bucket);
+
+// commit the pending log objec tto the log bucket
+// and create a new pending log object
+// if "must_commit" is "false" the function will return success even if the pending log object was not committed
+int rollover_logging_object(const configuration& conf,
+    const std::unique_ptr<rgw::sal::Bucket>& bucket,
+    std::string& obj_name,
+    const DoutPrefixProvider *dpp,
+    optional_yield y,
+    bool must_commit,
+    RGWObjVersionTracker* objv_tracker);
+
+// return the oid of the object holding the name of the temporary logging object
+// bucket - log bucket
+// prefix - logging prefix from configuration. should be used when multiple buckets log into the same log bucket
+std::string object_name_oid(const rgw::sal::Bucket* bucket, const std::string& prefix);
+
+// log a bucket logging record according to type
+// configuration is fetched from bucket attributes
+// if no configuration exists, or if type does not match the function return zero (success)
+int log_record(rgw::sal::Driver* driver,
+    LoggingType type,
+    const sal::Object* obj,
+    const req_state* s, 
+    const std::string& op_name, 
+    const std::string& etag, 
+    size_t size, 
+    const DoutPrefixProvider *dpp, 
+    optional_yield y, 
+    bool async_completion,
+    bool log_source_bucket);
+} // namespace rgw::bucketlogging
+

src/rgw/rgw_rest_bucket_logging.cc
@@ -1,0 +1,219 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#include "common/dout.h"
+#include "rgw_op.h"
+#include "rgw_rest.h"
+#include "rgw_rest_s3.h"
+#include "rgw_arn.h"
+#include "rgw_auth_s3.h"
+#include "rgw_url.h"
+#include "rgw_bucket_logging.h"
+#include "rgw_rest_bucket_logging.h"
+
+#define dout_context g_ceph_context
+#define dout_subsys ceph_subsys_rgw
+
+namespace {
+  int verify_bucket_logging_params(const DoutPrefixProvider* dpp,  const req_state* s) {
+    bool exists;
+    const auto no_value = s->info.args.get("logging", &exists);
+    if (!exists) {
+      ldpp_dout(dpp, 1) << "ERROR: missing required param 'logging'" << dendl;
+      return -EINVAL;
+    } 
+    if (no_value.length() > 0) {
+      ldpp_dout(dpp, 1) << "ERROR: param 'logging' should not have any value" << dendl;
+      return -EINVAL;
+    }
+    if (s->bucket_name.empty()) {
+      ldpp_dout(dpp, 1) << "ERROR: logging request must be on a bucket" << dendl;
+      return -EINVAL;
+    }
+    return 0;
+  }
+}
+
+// GET /<bucket name>/?logging
+// reply is XML encoded
+class RGWGetBucketLoggingOp : public RGWOp {
+  rgw::bucketlogging::configuration configuration;
+
+public:
+  int verify_permission(optional_yield y) override {
+    auto [has_s3_existing_tag, has_s3_resource_tag] = rgw_check_policy_condition(this, s, false);
+    if (has_s3_resource_tag)
+      rgw_iam_add_buckettags(this, s);
+
+    if (!verify_bucket_permission(this, s, rgw::IAM::s3GetBucketLogging)) {
+      return -EACCES;
+    }
+
+    return 0;
+  }
+
+  void execute(optional_yield y) override {
+    op_ret = verify_bucket_logging_params(this, s);
+    if (op_ret < 0) {
+      return;
+    }
+
+    std::unique_ptr<rgw::sal::Bucket> bucket;
+    op_ret = driver->load_bucket(this, rgw_bucket(s->bucket_tenant, s->bucket_name),
+                                 &bucket, y);
+    if (op_ret < 0) {
+      ldpp_dout(this, 1) << "ERROR: failed to get bucket '" << 
+        (s->bucket_tenant.empty() ? s->bucket_name : s->bucket_tenant + ":" + s->bucket_name) << 
+        "' info, ret = " << op_ret << dendl;
+      return;
+    }
+    if (auto iter = bucket->get_attrs().find(RGW_ATTR_BUCKET_LOGGING); iter != bucket->get_attrs().end()) {
+      try {
+        configuration.enabled = true;
+        decode(configuration, iter->second);
+      } catch (buffer::error& err) {
+        ldpp_dout(this, 1) << "ERROR: failed to decode attribute '" << RGW_ATTR_BUCKET_LOGGING 
+          << "'. error: " << err.what() << dendl;
+        op_ret = -EIO;
+        return;
+      }
+    } else {
+      ldpp_dout(this, 5) << "WARNING: no logging configuration on bucket '" << bucket->get_name() << "'" << dendl;
+      return;
+    }
+    ldpp_dout(this, 20) << "INFO: found logging configuration on bucket '" << bucket->get_name() << "'" 
+      << "'. configuration: " << configuration.to_json_str() << dendl;
+  }
+
+  void send_response() override {
+    dump_errno(s);
+    end_header(s, this, to_mime_type(s->format));
+    dump_start(s);
+
+    s->formatter->open_object_section_in_ns("BucketLoggingStatus", XMLNS_AWS_S3);
+    configuration.dump_xml(s->formatter);
+    s->formatter->close_section();
+    rgw_flush_formatter_and_reset(s, s->formatter);
+  }
+  const char* name() const override { return "get_bucket_logging"; }
+  RGWOpType get_type() override { return RGW_OP_GET_BUCKET_LOGGING; }
+  uint32_t op_mask() override { return RGW_OP_TYPE_READ; }
+};
+
+// PUT /<bucket name>/?logging
+// actual configuration is XML encoded in the body of the message
+class RGWPutBucketLoggingOp : public RGWDefaultResponseOp {
+  int verify_permission(optional_yield y) override {
+    auto [has_s3_existing_tag, has_s3_resource_tag] = rgw_check_policy_condition(this, s, false);
+    if (has_s3_resource_tag)
+      rgw_iam_add_buckettags(this, s);
+
+    if (!verify_bucket_permission(this, s, rgw::IAM::s3PutBucketLogging)) {
+      return -EACCES;
+    }
+
+    return 0;
+  }
+
+  const char* name() const override { return "put_bucket_logging"; }
+  RGWOpType get_type() override { return RGW_OP_PUT_BUCKET_LOGGING; }
+  uint32_t op_mask() override { return RGW_OP_TYPE_WRITE; }
+
+  void execute(optional_yield y) override { 
+    op_ret = verify_bucket_logging_params(this, s);
+    if (op_ret < 0) {
+      return;
+    }
+
+    const auto max_size = s->cct->_conf->rgw_max_put_param_size;
+    bufferlist data;
+    std::tie(op_ret, data) = read_all_input(s, max_size, false);
+    if (op_ret < 0) {
+      ldpp_dout(this, 1) << "ERROR: failed to read XML logging payload, ret = " << op_ret << dendl;
+      return;
+    }
+    if (data.length() == 0) {
+      ldpp_dout(this, 1) << "ERROR: XML logging payload missing" << dendl;
+      op_ret = -EINVAL;
+      return;
+    }
+
+    RGWXMLDecoder::XMLParser parser;
+    if (!parser.init()){
+      ldpp_dout(this, 1) << "ERROR: failed to initialize XML parser" << dendl;
+      op_ret = -EINVAL;
+      return;
+    }
+    if (!parser.parse(data.c_str(), data.length(), 1)) {
+      ldpp_dout(this, 1) << "ERROR: failed to parse XML logging payload" << dendl;
+      op_ret = -ERR_MALFORMED_XML;
+      return;
+    }
+    rgw::bucketlogging::configuration configuration;
+    configuration.default_obj_roll_time = get_cct()->_conf->rgw_bucket_logging_obj_roll_time;
+    try {
+      RGWXMLDecoder::decode_xml("BucketLoggingStatus", configuration, &parser, true);
+    } catch (RGWXMLDecoder::err& err) {
+      ldpp_dout(this, 1) << "ERROR: failed to parse XML logging payload. error: " << err << dendl;
+      op_ret = -ERR_MALFORMED_XML;
+      return;
+    }
+
+    std::unique_ptr<rgw::sal::Bucket> bucket;
+    op_ret = driver->load_bucket(this, rgw_bucket(s->bucket_tenant, s->bucket_name),
+                                 &bucket, y);
+    if (op_ret < 0) {
+      ldpp_dout(this, 1) << "ERROR: failed to get bucket '" << s->bucket_name << "', ret = " << op_ret << dendl;
+      return;
+    }
+
+
+    auto& attrs = bucket->get_attrs();
+    if (!configuration.enabled) {
+      if (auto iter = attrs.find(RGW_ATTR_BUCKET_LOGGING); iter != attrs.end()) {
+        attrs.erase(iter);
+      }
+    } else {
+      std::unique_ptr<rgw::sal::Bucket> target_bucket;
+      op_ret = driver->load_bucket(this, rgw_bucket(s->bucket_tenant, configuration.target_bucket),
+                                   &target_bucket, y);
+      if (op_ret < 0) {
+        ldpp_dout(this, 1) << "ERROR: failed to get target bucket '" << configuration.target_bucket << "', ret = " << op_ret << dendl;
+        return;
+      }
+      const auto& target_attrs = target_bucket->get_attrs();
+      if (target_attrs.find(RGW_ATTR_BUCKET_LOGGING) != target_attrs.end()) {
+        // target bucket must not have logging set on it
+        ldpp_dout(this, 1) << "ERROR: logging target bucket '" << configuration.target_bucket << "', is configured with bucket logging" << dendl;
+        op_ret = -EINVAL;
+        return;
+      }
+      // TODO: verify target bucket does not have encryption
+      bufferlist conf_bl;
+      encode(configuration, conf_bl);
+      attrs[RGW_ATTR_BUCKET_LOGGING] = conf_bl;
+      // TODO: should we add attribute to target bucket indicating it is target to bucket logging?
+      // if we do, how do we maintain it when bucket logging changes?
+    }
+    // TODO: use retry_raced_bucket_write from rgw_op.cc
+    op_ret = bucket->merge_and_store_attrs(this, attrs, y);
+    if (op_ret < 0) {
+      ldpp_dout(this, 1) << "ERROR: failed to set logging attribute '" << RGW_ATTR_BUCKET_LOGGING << "' to bucket '" << 
+        bucket->get_name() << "', ret = " << op_ret << dendl;
+      return;
+    }
+
+    ldpp_dout(this, 20) << "INFO: " << (configuration.enabled ? "wrote" : "removed")
+      << " logging configuration. bucket '" << bucket->get_name() << "'. configuration: " <<
+      configuration.to_json_str() << dendl;
+  }
+};
+
+RGWOp* RGWHandler_REST_BucketLogging_S3::create_put_op() {
+  return new RGWPutBucketLoggingOp();
+}
+
+RGWOp* RGWHandler_REST_BucketLogging_S3::create_get_op() {
+  return new RGWGetBucketLoggingOp();
+}
+

examples/rgw/boto3/bucket_logging.py
@@ -1,0 +1,50 @@
+#!/usr/bin/python
+
+import boto3
+import sys
+
+if len(sys.argv) != 3:
+    print('Usage: ' + sys.argv[0] + ' <bucket> <target bucket>')
+    sys.exit(1)
+
+# bucket name as first argument
+bucket = sys.argv[1]
+# target bucket name as the 2nd argument
+target_bucket = sys.argv[2]
+
+# endpoint and keys from vstart
+endpoint = 'http://127.0.0.1:8000'
+access_key='0555b35654ad1656d804'
+secret_key='h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q==' # notsecret
+
+client = boto3.client('s3',
+        endpoint_url=endpoint,
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key)
+
+
+# create the source bucket
+response = client.create_bucket(Bucket=bucket)
+print(response)
+
+# create the target bucket
+response = client.create_bucket(Bucket=target_bucket)
+print(response)
+
+bucket_logging_conf = {'LoggingEnabled': {
+    'TargetBucket': target_bucket,
+    'TargetPrefix': 'log/',
+    'TargetObjectKeyFormat': {
+      'SimplePrefix': {}
+    },
+    'ObjectRollTime': 60,
+    'LoggingType': 'Journal',
+  }
+}
+
+response = client.put_bucket_logging(Bucket=bucket, BucketLoggingStatus=bucket_logging_conf)
+print(response)
+
+response = client.get_bucket_logging(Bucket=bucket)
+print(response)
+

src/common/options/rgw.yaml.in
@@ -4250,3 +4250,13 @@ options:
   flags:
   - startup
   with_legacy: true
+- name: rgw_bucket_logging_obj_roll_time
+  type: uint 
+  level: advanced
+  desc: Default time in seconds for the bucket logging object to roll
+  long_desc: Object roll time can be provided in the bucket logging configuration.
+    If not provided, this value will be used.
+  default: 300
+  services:
+  - rgw
+  with_legacy: true

src/rgw/CMakeLists.txt
@@ -151,6 +151,8 @@ set(librgw_common_srcs
   rgw_data_access.cc
   driver/rados/account.cc
   driver/rados/buckets.cc
+  rgw_bucket_logging.cc
+  rgw_rest_bucket_logging.cc
   driver/rados/cls_fifo_legacy.cc
   driver/rados/group.cc
   driver/rados/groups.cc

commit 2fd408ec1595a32cc4616b921b093de8fbf8aa3a

rgw/logging: extract tenant from bucket name on admin flush
test instructions:
https://gist.github.com/yuvalif/adfa186fdbe9ad4c5b689753a15ec480

bug was introduced in: 790c38eacc52cc4c14beb48fca8b204235632793

Resolves: rhbz#2360666

Signed-off-by: Yuval Lifshitz <ylifshit@ibm.com>
(cherry picked from commit 604780dbedd643b7b633939182bd072e5b9c9cc1)

src/rgw/rgw_admin.cc
@@ -7600,27 +7600,19 @@ int main(int argc, const char **argv)
     if (ret < 0) {
       return -ret;
     }
-    const auto& bucket_attrs = bucket->get_attrs();
-    auto iter = bucket_attrs.find(RGW_ATTR_BUCKET_LOGGING);
-    if (iter == bucket_attrs.end()) {
-      cerr << "WARNING: no logging configured on bucket" << std::endl;
+
+    rgw::bucketlogging::configuration configuration;
+    std::unique_ptr<rgw::sal::Bucket> target_bucket;
+    ret =  rgw::bucketlogging::get_target_and_conf_from_source(dpp(), driver, bucket.get(), tenant, configuration, target_bucket, null_yield);
+    if (ret < 0 && ret != -ENODATA) {
+      cerr << "ERROR: failed to get target bucket and logging conf from source bucket '"
+        << bucket_name << "': " << cpp_strerror(-ret) << std::endl;
+      return -ret;
+    } else if (ret == -ENODATA) {
+      cerr << "ERROR: bucket '" << bucket_name << "' does not have logging enabled" << std::endl;
       return 0;
     }
-    rgw::bucketlogging::configuration configuration;
-    try {
-      configuration.enabled = true;
-      decode(configuration, iter->second);
-    } catch (buffer::error& err) {
-      cerr << "ERROR: failed to decode logging attribute '" << RGW_ATTR_BUCKET_LOGGING
-        << "'. error: " << err.what() << std::endl;
-      return  EINVAL;
-    }
-    std::unique_ptr<rgw::sal::Bucket> target_bucket;
-    ret = init_bucket(tenant, configuration.target_bucket, "", &target_bucket);
-    if (ret < 0) {
-      cerr << "ERROR: failed to get target logging bucket '" << configuration.target_bucket << "'" << std::endl;
-      return -ret;
-    }
+
     std::string obj_name;
     RGWObjVersionTracker objv_tracker;
     ret = target_bucket->get_logging_object_name(obj_name, configuration.target_prefix, null_yield, dpp(), &objv_tracker);

src/rgw/rgw_bucket_logging.cc
@@ -904,5 +904,44 @@ int verify_target_bucket_attributes(const DoutPrefixProvider* dpp, rgw::sal::Buc
   return 0;
 }
 
+int get_target_and_conf_from_source(
+    const DoutPrefixProvider* dpp,
+    rgw::sal::Driver* driver,
+    rgw::sal::Bucket* src_bucket,
+    const std::string& tenant,
+    configuration& configuration,
+    std::unique_ptr<rgw::sal::Bucket>& target_bucket,
+    optional_yield y) {
+    const auto src_bucket_id = src_bucket->get_key();
+    const auto& bucket_attrs = src_bucket->get_attrs();
+    auto iter = bucket_attrs.find(RGW_ATTR_BUCKET_LOGGING);
+    if (iter == bucket_attrs.end()) {
+      ldpp_dout(dpp, 1) << "WARNING: no logging configured on bucket '" << src_bucket_id << "'" << dendl;
+      return -ENODATA;
+    }
+    try {
+      configuration.enabled = true;
+      auto bl_iter = iter->second.cbegin();
+      decode(configuration, bl_iter);
+    } catch (buffer::error& err) {
+      ldpp_dout(dpp, 1) << "WARNING: failed to decode logging attribute '" << RGW_ATTR_BUCKET_LOGGING
+        << "' for bucket '" << src_bucket_id << "', error: " << err.what() << dendl;
+      return -EINVAL;
+    }
+
+    rgw_bucket target_bucket_id;
+    if (const auto ret = get_bucket_id(configuration.target_bucket, tenant, target_bucket_id); ret < 0) {
+      ldpp_dout(dpp, 1) << "ERROR: failed to parse target bucket '" << configuration.target_bucket << "', ret = " << ret << dendl;
+      return ret;
+    }
+
+    if (const auto ret = driver->load_bucket(dpp, target_bucket_id,
+                                 &target_bucket, y); ret < 0) {
+      ldpp_dout(dpp, 1) << "ERROR: failed to get target bucket '" << target_bucket_id << "', ret = " << ret << dendl;
+      return ret;
+    }
+    return 0;
+}
+
 } // namespace rgw::bucketlogging
 

src/rgw/rgw_bucket_logging.h
@@ -262,5 +262,19 @@ int verify_target_bucket_policy(const DoutPrefixProvider* dpp,
 // - encryption
 int verify_target_bucket_attributes(const DoutPrefixProvider* dpp, rgw::sal::Bucket* target_bucket);
 
+// given a source bucket this function is parsing the configuration object
+// extracting the target bucket and load it
+// the log bucket tenant is taken from configuration
+// however, if not explicitly set there, it is taken from the tenant parameter
+// both configuration and target bucket are returned by reference
+int get_target_and_conf_from_source(
+    const DoutPrefixProvider* dpp,
+    rgw::sal::Driver* driver,
+    rgw::sal::Bucket* src_bucket,
+    const std::string& tenant,
+    configuration& configuration,
+    std::unique_ptr<rgw::sal::Bucket>& target_bucket,
+    optional_yield y);
+
 } // namespace rgw::bucketlogging
 

src/rgw/rgw_rest_bucket_logging.cc
@@ -327,34 +327,7 @@ class RGWPostBucketLoggingOp : public RGWDefaultResponseOp {
         return ret;
       }
     }
-    const auto src_bucket_id = src_bucket->get_key();
-    const auto& bucket_attrs = src_bucket->get_attrs();
-    auto iter = bucket_attrs.find(RGW_ATTR_BUCKET_LOGGING);
-    if (iter == bucket_attrs.end()) {
-      ldpp_dout(this, 1) << "WARNING: no logging configured on bucket '" << src_bucket_id << "'" << dendl;
-      return 0;
-    }
-    try {
-      configuration.enabled = true;
-      decode(configuration, iter->second);
-    } catch (buffer::error& err) {
-      ldpp_dout(this, 1) << "WARNING: failed to decode logging attribute '" << RGW_ATTR_BUCKET_LOGGING
-        << "' for bucket '" << src_bucket_id << "', error: " << err.what() << dendl;
-      return -EINVAL;
-    }
-
-    rgw_bucket target_bucket_id;
-    if (const auto ret = rgw::bucketlogging::get_bucket_id(configuration.target_bucket, s->bucket_tenant, target_bucket_id); ret < 0) {
-      ldpp_dout(this, 1) << "ERROR: failed to parse target bucket '" << configuration.target_bucket << "', ret = " << ret << dendl;
-      return ret;
-    }
-
-    if (const auto ret = driver->load_bucket(this, target_bucket_id,
-                                 &target_bucket, y); ret < 0) {
-      ldpp_dout(this, 1) << "ERROR: failed to get target bucket '" << target_bucket_id << "', ret = " << ret << dendl;
-      return ret;
-    }
-    return 0;
+    return rgw::bucketlogging::get_target_and_conf_from_source(this, driver, src_bucket.get(), s->bucket_tenant, configuration, target_bucket, y);
   }
 
   int verify_permission(optional_yield y) override {