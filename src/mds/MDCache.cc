mkogan1/ceph

commit a8a103c311133ea3ec43105ed0596846a5993d24

mds: do not path traverse a damaged dirfrag
Fixes: https://tracker.ceph.com/issues/69695
Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
(cherry picked from commit 3243af47d60558227263203cdc4f9b9f0ebb3f5e)
(cherry picked from commit 3869e2c7392121fc4a06b0e722d926889306124b)
Resolves: rhbz#2343968

src/mds/MDCache.cc
@@ -8448,6 +8448,13 @@ int MDCache::path_traverse(const MDRequestRef& mdr, MDSContextFactory& cf,
       }
     }
     ceph_assert(curdir);
+
+    if (mds->damage_table.is_dirfrag_damaged(curdir)) {
+      dout(4) << "traverse: stopped lookup at dirfrag "
+              << *curdir << "/" << path[depth] << " snap=" << snapid << dendl;
+      return -CEPHFS_EIO;
+    }
+
     if (pdir) {
       *pdir = curdir;
     }

commit 4a05849fcd8f0a38b28e92e8340c1a070ab29986

mds: fix broken define by backport
Fixes: a8a103c311133ea3ec43105ed0596846a5993d24
Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
Resolves: rhbz#2343968

src/mds/MDCache.cc
@@ -8452,7 +8452,7 @@ int MDCache::path_traverse(const MDRequestRef& mdr, MDSContextFactory& cf,
     if (mds->damage_table.is_dirfrag_damaged(curdir)) {
       dout(4) << "traverse: stopped lookup at dirfrag "
               << *curdir << "/" << path[depth] << " snap=" << snapid << dendl;
-      return -CEPHFS_EIO;
+      return -EIO;
     }
 
     if (pdir) {