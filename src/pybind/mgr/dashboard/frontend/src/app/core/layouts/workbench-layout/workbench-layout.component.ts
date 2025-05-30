mkogan1/ceph

commit 077a0d0482329b8b4451879f802d1b9af78a9f57

mgr/dashboard: adapt call-home module enable code
Resolves: rhbz#2365915

Signed-off-by: Nizamudeen A <nia@redhat.com>

src/pybind/mgr/dashboard/frontend/src/app/shared/components/call-home-modal/call-home-modal.component.ts
@@ -4,8 +4,7 @@ import { MgrModuleService } from '../../api/mgr-module.service';
 import { NotificationService } from '../../services/notification.service';
 import { NotificationType } from '../../enum/notification-type.enum';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
-import { BehaviorSubject, Observable, timer } from 'rxjs';
-import { CallHomeNotificationService } from '../../services/call-home-notification.service';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { CdFormGroup } from '../../forms/cd-form-group';
 import { FormControl, Validators } from '@angular/forms';
 import { CallHomeService } from '../../api/call-home.service';
@@ -41,7 +40,6 @@ export class CallHomeModalComponent extends CdForm implements OnInit {
     public activeModal: NgbActiveModal,
     private mgrModuleService: MgrModuleService,
     private notificationService: NotificationService,
-    private callHomeNotificationService: CallHomeNotificationService,
     private callHomeSerive: CallHomeService,
     private textToDownloadService: TextToDownloadService
   ) {
@@ -121,64 +119,16 @@ export class CallHomeModalComponent extends CdForm implements OnInit {
   }
 
   callHome(enable = true) {
-    const fnWaitUntilReconnected = () => {
-      timer(2000).subscribe(() => {
-        // Trigger an API request to check if the connection is
-        // re-established.
-        this.mgrModuleService.list().subscribe(
-          () => {
-            // Resume showing the notification toasties.
-            this.notificationService.suspendToasties(false);
-            // Unblock the whole UI.
-            this.blockUI.stop();
-            // Reload the data table content.
-            if (enable) {
-              this.notificationService.show(
-                NotificationType.success,
-                $localize`Activated IBM Call Home Agent`
-              );
-              this.callHomeNotificationService.setVisibility(false);
-              this.activeModal.close();
-            } else {
-              this.notificationService.show(
-                NotificationType.success,
-                $localize`Deactivated IBM Call Home Agent`
-              );
-              this.activeModal.close();
-            }
-          },
-          () => {
-            fnWaitUntilReconnected();
-          }
-        );
-      });
-    };
-
-    if (enable) {
-      this.mgrModuleService.enable('call_home_agent').subscribe(
-        () => undefined,
-        () => {
-          // Suspend showing the notification toasties.
-          this.notificationService.suspendToasties(true);
-          // Block the whole UI to prevent user interactions until
-          // the connection to the backend is reestablished
-          this.blockUI.start($localize`Reconnecting, please wait ...`);
-          fnWaitUntilReconnected();
-        }
-      );
-    } else {
-      this.mgrModuleService.disable('call_home_agent').subscribe(
-        () => undefined,
-        () => {
-          // Suspend showing the notification toasties.
-          this.notificationService.suspendToasties(true);
-          // Block the whole UI to prevent user interactions until
-          // the connection to the backend is reestablished
-          this.blockUI.start($localize`Reconnecting, please wait ...`);
-          fnWaitUntilReconnected();
-        }
-      );
-    }
+    this.mgrModuleService.updateModuleState(
+      'call_home_agent',
+      !enable,
+      null,
+      '',
+      enable ? $localize`Activated IBM Call Home Agent`
+       : $localize`Deactivated IBM Call Home Agent`,
+      false,
+      this.activeModal
+    );
   }
 
   testConnectivity() {

src/pybind/mgr/dashboard/frontend/src/app/shared/api/mgr-module.service.ts
@@ -8,6 +8,7 @@ import { TableComponent } from '../datatable/table/table.component';
 import { Router } from '@angular/router';
 import { MgrModuleInfo } from '../models/mgr-modules.interface';
 import { NotificationType } from '../enum/notification-type.enum';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable({
   providedIn: 'root'
@@ -85,7 +86,8 @@ export class MgrModuleService {
     table: TableComponent = null,
     navigateTo: string = '',
     notificationText?: string,
-    navigateByUrl?: boolean
+    navigateByUrl?: boolean,
+    activeModal?: NgbActiveModal
   ): void {
     let $obs;
     const fnWaitUntilReconnected = () => {
@@ -102,6 +104,8 @@ export class MgrModuleService {
             if (table) {
               table.refreshBtn();
             }
+
+            if (activeModal) activeModal.close();
 
             if (notificationText) {
               this.notificationService.show(

commit dc129e2583edf9e7f745cc2676ea344c1a63ed15

mgr/dashboard: fix call home banner's visibility
call home banner is not going away even after enabling the module.
Regression from https://gitlab.cee.redhat.com/ceph/ceph/-/commit/077a0d0482329b8b4451879f802d1b9af78a9f57

Resolves: rhbz#2369133

Signed-off-by: Nizamudeen A <nia@redhat.com>

src/pybind/mgr/dashboard/frontend/src/app/core/layouts/workbench-layout/workbench-layout.component.ts
@@ -50,6 +50,8 @@ export class WorkbenchLayoutComponent implements OnInit, OnDestroy {
     if (this.permissions.configOpt.read) {
       this.subs.add(this.multiClusterService.startPolling());
       this.subs.add(this.multiClusterService.startClusterTokenStatusPolling());
+      this.subs.add(this.summaryService.startPolling());
+      this.subs.add(this.taskManagerService.init(this.summaryService));
 
       if (this.environment.build === 'ibm') {
         this.subs.add(
@@ -72,8 +74,6 @@ export class WorkbenchLayoutComponent implements OnInit, OnDestroy {
       }
     }
 
-    this.subs.add(this.summaryService.startPolling());
-    this.subs.add(this.taskManagerService.init(this.summaryService));
 
     this.subs.add(
       this.authStorageService.isPwdDisplayed$.subscribe((isDisplayed) => {

src/pybind/mgr/dashboard/frontend/src/app/shared/components/call-home-modal/call-home-modal.component.ts
@@ -13,6 +13,7 @@ import { TextToDownloadService } from '../../services/text-to-download.service';
 import { ConnectivityStatus } from '../../models/call-home.model';
 import { switchMap } from 'rxjs/operators';
 import { Icons } from '../../enum/icons.enum';
+import { CallHomeNotificationService } from '../../services/call-home-notification.service';
 
 @Component({
   selector: 'cd-call-home-modal',
@@ -41,7 +42,8 @@ export class CallHomeModalComponent extends CdForm implements OnInit {
     private mgrModuleService: MgrModuleService,
     private notificationService: NotificationService,
     private callHomeSerive: CallHomeService,
-    private textToDownloadService: TextToDownloadService
+    private textToDownloadService: TextToDownloadService,
+    private callHomeNotificationService: CallHomeNotificationService
   ) {
     super();
   }
@@ -113,8 +115,9 @@ export class CallHomeModalComponent extends CdForm implements OnInit {
         customer_country_code: countryCode
       })
       .subscribe({
+        next: () => this.callHome(),
         error: () => this.callHomeForm.setErrors({ cdSubmitButton: true }),
-        complete: () => this.callHome()
+        complete: () => this.callHomeNotificationService.setVisibility(false)
       });
   }
 
@@ -129,6 +132,8 @@ export class CallHomeModalComponent extends CdForm implements OnInit {
       false,
       this.activeModal
     );
+
+    if (!enable) this.callHomeNotificationService.setVisibility(true);
   }
 
   testConnectivity() {