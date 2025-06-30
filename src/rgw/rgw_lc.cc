eebeb7eed576bc14a8a679ae6d4bbd2fb695cd04
Matt Benjamin <mbenjamin@redhat.com>
2025-06-30 10:26:25 -0400

    rgwlc: fix removal of delete markers (SAL)
    S3 delete markers do not have head objects, and SAL's Object::load_obj_state()
    returns -ENOENT in this case.  Handle this case in LC's remove_expired_obj().
    
    Fixes: https://tracker.ceph.com/issues/70853
    Resolves: rhbz#2381933
    Resolves: rhbz#2360974
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 224821147f2664e54f81b0bb93ccd23669f31f04)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_lc.cc
+++ src/rgw/rgw_lc.cc
@@ -577,7 +577,19 @@ static int remove_expired_obj(
   string etag;
   ret = obj->get_obj_state(dpp, &obj_state, null_yield, true);
   if (ret < 0) {
+#if 0
     return ret;
+#else
+    /* for delete markers, we expect load_obj_state() to "fail"
+     * with -ENOENT */
+    if (! (o.is_delete_marker() &&
+	   (ret == -ENOENT))) {
+      ldpp_dout(oc.dpp, 0) <<
+	fmt::format("ERROR: get_obj_state() failed in {} for object k={} error r={}",
+		    __func__, oc.o.key.to_string(), ret) << dendl;
+      return ret;
+    }
+#endif
   }
   auto iter = obj_state->attrset.find(RGW_ATTR_ETAG);
   if (iter != obj_state->attrset.end()) {
@@ -1271,13 +1283,14 @@ public:
 			<< oc.wq->thr_name() << dendl;
       return false;
     }
+    /* don't remove the delete marker if that would expose a non-current
+     * version as current */
     if (oc.next_has_same_name(o.key.name)) {
       ldpp_dout(dpp, 20) << __func__ << "(): key=" << o.key
-			<< ": next is same object, skipping "
+			<< ": dm expiration would expose a non-current version, skipping "
 			<< oc.wq->thr_name() << dendl;
       return false;
     }
-
     *exp_time = real_clock::now();
 
     return true;
