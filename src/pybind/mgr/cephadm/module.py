40e86ab86b116da4e955e1b34336419518ccb1b7
Adam King <adking@redhat.com>
2025-07-30 15:49:20 -0400

    mgr/cephadm: add interval control for stray daemon checks
    Primarily to avoid running list_servers (which we kind of
    need to do stray daemon checks since the whole point is
    to check against a source that isn't cephadm). It was
    found on larger clusters calling into list_servers
    often can cause issues with the core ceph mgr
    
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit ee0364761e1ee29e6ad527dddd0eafc01c1f1aaa)
    (cherry picked from commit d0db16af74f2cfb5f2a47fa0f6020fb760f7735c)
    
    Conflicts:
    	src/pybind/mgr/cephadm/module.py
    	src/pybind/mgr/cephadm/serve.py
    
    Resolves: rhbz#2390033

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -213,6 +213,12 @@ class CephadmOrchestrator(orchestrator.Orchestrator, MgrModule,
             desc='how frequently to perform a host check',
         ),
         Option(
+            'stray_daemon_check_interval',
+            type='secs',
+            default=30 * 60,
+            desc='how frequently cephadm should check for the presence of stray daemons',
+        ),
+        Option(
             'mode',
             type='str',
             enum_allowed=['root', 'cephadm-package'],
@@ -540,6 +546,7 @@ class CephadmOrchestrator(orchestrator.Orchestrator, MgrModule,
             self.daemon_cache_timeout = 0
             self.facts_cache_timeout = 0
             self.host_check_interval = 0
+            self.stray_daemon_check_interval = 0
             self.max_count_per_host = 0
             self.mode = ''
             self.container_image_base = ''
@@ -717,6 +724,7 @@ class CephadmOrchestrator(orchestrator.Orchestrator, MgrModule,
         self.daemon_removal_queue = DaemonRemovalQueue()
 
         self.created_ganesha_pool = False
+        self.last_stray_daemon_check: Optional[datetime.datetime] = None
 
     def shutdown(self) -> None:
         self.log.debug('shutdown')
