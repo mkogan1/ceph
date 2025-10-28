ca437f4798957860bf107182379cc14d4ea69658
Pritha Srivastava <prsrivas@redhat.com>
2025-10-28 21:33:25 +0530

    rgw/sts: RoleApplier respects ACL grants instead of returning 0 from RoleApplier::get_perms_from_aclspec(), call rgw_perms_from_aclspec_default_strategy() to check for matching ACL grants
    this fixes a regression from commit b021d0f
    when identity/session policy implicitly deny a request. previously,
    these requests could be granted based on bucket ownership
    
    add the missing RoleApplier support to grant access based on ACLs
    
    Fixes: https://tracker.ceph.com/issues/73659
    Resolves: rhbz#2406837
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_auth.h
+++ src/rgw/rgw_auth.h
@@ -793,17 +793,13 @@ public:
       is_system_request(is_system_request) {}
 
   ACLOwner get_aclowner() const override;
-  uint32_t get_perms_from_aclspec(const DoutPrefixProvider* dpp, const aclspec_t& aclspec) const override {
-    return 0;
-  }
+  uint32_t get_perms_from_aclspec(const DoutPrefixProvider* dpp, const aclspec_t& aclspec) const override;
   bool is_admin_of(const rgw_owner& o) const override {
     return false;
   }
   bool is_owner_of(const rgw_owner& o) const override;
   bool is_identity(const Principal& p) const override;
-  uint32_t get_perm_mask() const override {
-    return RGW_PERM_NONE; 
-  }
+  uint32_t get_perm_mask() const override;
   void to_str(std::ostream& out) const override;
   auto load_acct_info(const DoutPrefixProvider* dpp) const -> std::unique_ptr<rgw::sal::User> override; /* out */
   uint32_t get_identity_type() const override { return TYPE_ROLE; }
