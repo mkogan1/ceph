4ce6c93e776c5e9980aa538dae6e658f90d3ae94
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2026-03-16 13:06:38 +0530

    cephadm: Install dependent services during prepare-host only when specified
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>
    Resolves: IBMCEPH-13450

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -4482,7 +4482,8 @@ def command_prepare_host(ctx: CephadmContext) -> None:
         # the service
         check_time_sync(ctx)
 
-    install_required_services(ctx, pkg=pkg)
+    if hasattr(ctx, 'install_service_dependencies') and ctx.install_service_dependencies:
+        install_required_services(ctx, pkg=pkg)
 
     if 'expect_hostname' in ctx and ctx.expect_hostname and ctx.expect_hostname != get_hostname():
         logger.warning('Adjusting hostname from %s -> %s...' % (get_hostname(), ctx.expect_hostname))
@@ -5689,6 +5690,10 @@ def _get_parser():
     parser_prepare_host.add_argument(
         '--expect-hostname',
         help='Set hostname')
+    parser_prepare_host.add_argument(
+        '--install-service-dependencies',
+        action='store_true',
+        help='Installs the required system packages on the host that are needed for services to run.')
 
     parser_add_repo = subparsers.add_parser(
         'add-repo', help='configure package repository')
