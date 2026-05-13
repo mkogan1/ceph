e6ea591d83866854e37a6f4c01e2b01111bcd04f
Timothy Q Nguyen <timqn22@gmail.com>
2026-05-13 16:19:57 -0700

    src/cephadm: added ceph-exporter to post-rotate signal list
    As the title says this change simply adds ceph-exporter to a logrotate
    list which will ensure ceph-exporter will continue writing to a new log
    file even after log rotation. Currently no new log file will be written to
    and you will have to manually add ceph-exporter to logrotate.d.
    
    Signed-off-by: Timothy Q Nguyen <timqn22@gmail.com>
    (cherry picked from commit 2746a9b7c9d4155103bb3bdf69e9a68056edf819)

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -4553,6 +4553,7 @@ def install_base_units(ctx, fsid):
         should be harmless.
         """
         targets: List[str] = [
+            'ceph-exporter',
             'ceph-mon',
             'ceph-mgr',
             'ceph-mds',
