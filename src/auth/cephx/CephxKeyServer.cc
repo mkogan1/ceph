mkogan1/ceph

commit bae3d0dc2434efd671fdf2ff3cb4df51302c01ca

auth/cephx: generate session secret with common key type
During a rolling upgrade to a new CephX cipher type (e.g., migrating
from `aes` to `aes256k`), daemons that have already been upgraded and
rotated (such as `ceph-mgr` or `ceph-mds`) will request service tickets
to communicate with downstream daemons (such as `ceph-osd`).

Previously, `KeyServer::build_session_auth_info` operated under the
flawed assumption that services are always upgraded before clients. It
unconditionally prioritized the client's requested cipher type for the
session key. This breaks internal cluster communications because the
standard upgrade sequence (MON -> MGR -> MDS -> OSD) necessitates newer
clients talking to older services. An upgraded MGR or MDS would receive
a ticket containing an `aes256k` session key, which the older,
un-upgraded OSD cannot decode. This results in `malformed secret` errors
and dropped connections, which fractures OSD peering and breaks CephFS
operations.

This commit fixes the issue by clamping the session key cipher type to
the minimum (lowest common denominator) of the client's requested type
and the target service's rotating secret type. Because the service's
rotating secret accurately reflects the target daemon's current
cryptographic capabilities, this ensures the generated session key is
always mutually supported by both endpoints.

Once the downstream target services are subsequently upgraded and their
rotating keys update to the new cipher, subsequent ticket renewals will
automatically negotiate up to the stronger cipher.

Resolves: IBMCEPH-16408
Change-Id: 3ff04684-65c3-447b-bd92-cadd0dbaff9c
Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
(cherry picked from commit c7df927420c45a914f811079198d930d548f4406)

src/auth/cephx/CephxKeyServer.cc
@@ -588,7 +588,20 @@ int KeyServer::_build_session_auth_info(uint32_t service_id,
   info.ticket.init_timestamps(ceph_clock_now(), ttl);
   info.validity.set_from_double(ttl);
 
-  generate_secret(info.session_key, key_type);
+  /* The session key cipher must be supported by both the requesting client and
+   * the target service. During rolling upgrades, services are typically
+   * upgraded before external clients, but internal cluster daemons (such as
+   * the mgr) acting as RADOS clients may be upgraded before target services
+   * (such as osd). Therefore, we select the minimum (lowest common
+   * denominator) of the client's key type and the target service's rotating
+   * secret key type. This is also (even more) important to consider rolling
+   * upgrades of service daemons where two OSD or two MDS need to talk to each
+   * other.
+   */
+  int ktype = std::min(key_type.value_or(info.service_secret.get_type()),
+                       info.service_secret.get_type());
+
+  generate_secret(info.session_key, ktype);
 
   /* N.B.: the Monitor special cases cap retrieval via a call to
    * CephxServiceHandler::handle_request which fills in the
@@ -614,21 +627,9 @@ int KeyServer::build_session_auth_info(uint32_t service_id,
     return -EACCES;
   }
 
-  /* either use the provided key type, or the one that the service
-   * is using. As things are, there are two different cases:
-   * one that this is being called as a result of a client call
-   * and in which case we'll be provided with the client's key type.
-   * The second case is when the monitor generates tickets to
-   * connects to the manager, in which case we want to use
-   * the manager's key type. In any case, we assume that services
-   * are upgraded first before clients, so we prioritize client's
-   * key type over the service key type.
-   */
-  int ktype = key_type.value_or(info.service_secret.get_type());
-
   std::scoped_lock l{lock};
   return _build_session_auth_info(service_id, parent_ticket,
-                                  ktype, info, ttl);
+                                  key_type, info, ttl);
 }
 
 int KeyServer::build_session_auth_info(uint32_t service_id,

commit ab264bdbf63eb4dd0c26c0489fc27bfe542f86d0

auth/cephx: fix compiler error
Resolves: IBMCEPH-16408
Fixes: bae3d0dc2434efd671fdf2ff3cb4df51302c01ca
Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

src/auth/cephx/CephxKeyServer.cc
@@ -598,8 +598,8 @@ int KeyServer::_build_session_auth_info(uint32_t service_id,
    * upgrades of service daemons where two OSD or two MDS need to talk to each
    * other.
    */
-  int ktype = std::min(key_type.value_or(info.service_secret.get_type()),
-                       info.service_secret.get_type());
+  int ktype = std::min<int>(key_type.value_or(info.service_secret.get_type()),
+                            info.service_secret.get_type());
 
   generate_secret(info.session_key, ktype);
 